0872d265ead1b425ebe7eca5c17ac676ac93349e
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-03-26 20:29:27 +0100

    DrasylNode

---
+++ core/src/main/java/org/drasyl/node/DrasylNode.java
@@ -0,0 +1,72 @@
+package org.drasyl.node;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+public abstract class DrasylNode {
+    public DrasylNode() {
+        this(ConfigFactory.load());
+    }
+
+    public DrasylNode(Config config) {
+        // implement
+    }
+
+    abstract void onMessage(Message message);
+
+    abstract void onEvent(Event event);
+
+    public void send(Message message) {
+        // implement
+    }
+
+    public void close() {
+        // implement
+    }
+
+    public static void main(String[] args) {
+        // create node
+        DrasylNode node = new DrasylNode() {
+            @Override
+            void onMessage(Message message) {
+                System.out.println("Message received: " + message);
+            }
+
+            @Override
+            void onEvent(Event event) {
+                System.out.println("Event received: " + event);
+
+                switch (event.getCode()) {
+                    case NODE_ONLINE:
+                        System.out.println("Node is online with the following Id: " + event.getNode().getAddress());
+                        break;
+                    case NODE_OFFLINE:
+                        System.out.println("Node is offline");
+                        break;
+                    case PEER_P2P:
+                        System.out.println("Node can now send messages directly to " + event.getPeer().getAddress());
+                        break;
+                    case PEER_RELAY:
+                        System.out.println("Node can now send messages via a relay to " + event.getPeer().getAddress());
+                        break;
+                }
+            }
+        };
+
+//        // variante 1: Warte synchron bis node sendebereit ist und schicke dann nachricht
+//        while (!node.isOnline()) {
+//            Thread.sleep(1 * 1000L);
+//        }
+//
+//        Message message = null;
+//        node.send(message);
+//
+//        // variante 2: Warte asynchron bis ndoe sendebereit ist und schicke dann nachricht
+//        node.doOnOnline(() -> {
+//            Message message = null;
+//            node.send(message);
+//        });
+
+        node.close();
+    }
+}
---
+++ core/src/main/java/org/drasyl/node/Message.java
@@ -0,0 +1,50 @@
+package org.drasyl.node;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Message {
+    private final Object recipient;
+    private final byte[] payload;
+
+    public Message(Object recipient, byte[] payload) {
+        this.recipient = recipient;
+        this.payload = payload;
+    }
+
+    public Object getRecipient() {
+        return recipient;
+    }
+
+    public byte[] getPayload() {
+        return payload;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(recipient);
+        result = 31 * result + Arrays.hashCode(payload);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Message message = (Message) o;
+        return Objects.equals(recipient, message.recipient) &&
+                Arrays.equals(payload, message.payload);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "recipient=" + recipient +
+                ", payload=" + Arrays.toString(payload) +
+                '}';
+    }
+}
