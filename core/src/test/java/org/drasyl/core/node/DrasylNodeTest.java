e4df83ac42fda522e7a802fa03dcbd71dc994a2a
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-03-31 23:36:11 +0200

    More tests

--- core/src/test/java/org/drasyl/core/node/DrasylNodeTest.java
+++ core/src/test/java/org/drasyl/core/node/DrasylNodeTest.java
@@ -1,27 +1,28 @@
 package org.drasyl.core.node;
 
-import org.awaitility.Awaitility;
+import org.drasyl.core.crypto.CompressedPublicKey;
 import org.drasyl.core.node.models.Event;
+import org.drasyl.core.node.models.Event.Node;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mockito;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import static org.awaitility.Awaitility.await;
 import static org.drasyl.core.node.models.Event.Code.*;
-import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 public class DrasylNodeTest {
-    private Event.Node node;
+    private Node node;
+    private Event event;
+    private byte[] payload;
+    private CompressedPublicKey recipient;
 
     @BeforeEach
     void setUp() {
-        node = mock(Event.Node.class);
+        event = mock(Event.class);
+        node = mock(Node.class);
+        recipient = mock(CompressedPublicKey.class);
+        payload = new byte[]{ 0x4f };
     }
 
     @AfterEach
@@ -32,136 +33,211 @@ public class DrasylNodeTest {
     public void startShouldEmitOnlineEventOnSuccessfulSuperPeerRegistration() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
         drasylNode.start();
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_ONLINE, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_ONLINE, node, null));
     }
 
     @Test
     public void startShouldEmitIdentityCollisionEventIfIdentityIsAlreadyUsedByAnotherNode() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
         drasylNode.start();
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_IDENTITY_COLLISION, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_IDENTITY_COLLISION, node, null));
+    }
+
+    @Test
+    public void startShouldNotEmitAnyEventsIfNodeHasAlreadyBeenStarted() {
+        // FIXME: mock behaviour here
+
+        DrasylNode drasylNode = spy(new DrasylNode() {
+            @Override
+            void onMessage(byte[] payload) {
+            }
+
+            @Override
+            void onEvent(Event event) {
+            }
+        });
+        drasylNode.start();
+
+        verify(drasylNode, times(0)).onEvent(any());
     }
 
     @Test
     public void shutdownShouldEmitNormalTerminationEventOnSuccessfulSuperPeerDeregistration() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
         drasylNode.shutdown();
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_NORMAL_TERMINATION, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_NORMAL_TERMINATION, node, null));
     }
 
     @Test
     public void shutdownShouldEmitDeregisterFailedEventIfDeregistrationFromSuperPeerFailed() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
         drasylNode.shutdown();
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_DEREGISTER_FAILED, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_DEREGISTER_FAILED, node, null));
+    }
+
+    @Test
+    public void shutdownShouldNotEmitAnyEventsIfNodeHasAlreadyBeenShutDown() {
+        // FIXME: mock behaviour here
+
+        DrasylNode drasylNode = spy(new DrasylNode() {
+            @Override
+            void onMessage(byte[] payload) {
+            }
+
+            @Override
+            void onEvent(Event event) {
+            }
+        });
+        drasylNode.shutdown();
+
+        verify(drasylNode, times(0)).onEvent(any());
+    }
+
+    @Test
+    public void onEventShouldBeCalledForEveryEmittedEvent() {
+        // FIXME: mock behaviour here
+
+        DrasylNode drasylNode = spy(new DrasylNode() {
+            @Override
+            void onMessage(byte[] payload) {
+            }
+
+            @Override
+            void onEvent(Event event) {
+            }
+        });
+        drasylNode.shutdown();
+
+        verify(drasylNode).onEvent(event);
+    }
+
+    @Test
+    public void onMessageShouldBeCalledForEveryIncomingMessage() {
+        // FIXME: mock behaviour here
+
+        DrasylNode drasylNode = spy(new DrasylNode() {
+            @Override
+            void onMessage(byte[] payload) {
+            }
+
+            @Override
+            void onEvent(Event event) {
+            }
+        });
+
+        verify(drasylNode).onMessage(payload);
+    }
+
+    @Test
+    public void sendShouldSendTheMessageToTheRecipient() {
+        // FIXME: mock behaviour here
+
+        DrasylNode drasylNode = spy(new DrasylNode() {
+            @Override
+            void onMessage(byte[] payload) {
+            }
+
+            @Override
+            void onEvent(Event event) {
+            }
+        });
+        drasylNode.send(recipient, payload);
+
+        fail("make sure that DrasylNode has processed the message correctly");
     }
 
     @Test
     public void shouldEmitOfflineEventIfConnectionToSuperPeerIsLost() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_OFFLINE, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_OFFLINE, node, null));
     }
 
     @Test
     public void shouldEmitOnlineEventIfBrokenConnectionToSuperPeerReestablished() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_ONLINE, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_ONLINE, node, null));
     }
 
     @Test
     public void shouldEmitUnrecoverableErrorEventIfConnectionToSuperPeerCouldNotReestablished() {
         // FIXME: mock behaviour here
 
-        Collection<Event> events = new ArrayList<>();
-        DrasylNode drasylNode = new DrasylNode() {
+        DrasylNode drasylNode = spy(new DrasylNode() {
             @Override
             void onMessage(byte[] payload) {
             }
 
             @Override
             void onEvent(Event event) {
-                events.add(event);
             }
-        };
+        });
 
-        await().untilAsserted(() -> assertThat(events, contains(new Event(NODE_UNRECOVERABLE_ERROR, node, null))));
+        verify(drasylNode).onEvent(new Event(NODE_UNRECOVERABLE_ERROR, node, null));
     }
 }
