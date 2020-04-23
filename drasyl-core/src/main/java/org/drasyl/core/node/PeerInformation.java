5bde99bd8a13583c0451f8153ac39bb3c8182a36
Kevin Röbert <dev@xn--rb-fka.it>
2020-04-23 16:16:36 +0200

    Made classes thread safe

--- drasyl-core/src/main/java/org/drasyl/core/node/PeerInformation.java
+++ drasyl-core/src/main/java/org/drasyl/core/node/PeerInformation.java
@@ -1,47 +1,121 @@
+/*
+ * Copyright (c) 2020.
+ *
+ * This file is part of drasyl.
+ *
+ *  drasyl is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  drasyl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with drasyl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.drasyl.core.node;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Contains information on a specific peer (e.g. known endpoints and active connections).
+ *
+ * <p>
+ * This class is optimized for concurrent access and is thread-safe.
+ * </p>
  */
 public class PeerInformation {
-    private final List<URI> endpoints;
-    private final List<PeerConnection> connections;
+    private ReadWriteLock lock = new ReentrantReadWriteLock(true);
+
+    private final Set<URI> endpoints;
+    private final Set<PeerConnection> connections;
 
     public PeerInformation() {
-        this(new ArrayList<>(), new ArrayList<>());
+        this(new HashSet<>(), new HashSet<>());
     }
 
-    PeerInformation(List<URI> endpoints,
-                    List<PeerConnection> connections) {
+    PeerInformation(Set<URI> endpoints,
+                    Set<PeerConnection> connections) {
         this.endpoints = endpoints;
         this.connections = connections;
     }
 
-    public List<PeerConnection> getConnections() {
-        return connections;
+    public Set<PeerConnection> getConnections() {
+        try {
+            lock.readLock().lock();
+
+            return ImmutableSet.copyOf(connections);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public boolean addPeerConnection(PeerConnection... connections) {
-        return this.connections.addAll(List.of(connections));
+        Objects.requireNonNull(connections);
+
+        try {
+            lock.writeLock().lock();
+
+            return this.connections.addAll(Set.of(connections));
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public boolean removePeerConnection(PeerConnection... connections) {
-        return this.connections.removeAll(List.of(connections));
+        Objects.requireNonNull(connections);
+
+        try {
+            lock.writeLock().lock();
+
+            return this.connections.removeAll(Set.of(connections));
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
-    public List<URI> getEndpoints() {
-        return endpoints;
+    public Set<URI> getEndpoints() {
+        try {
+            lock.readLock().lock();
+
+            return ImmutableSet.copyOf(endpoints);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public boolean addEndpoint(URI... endpoints) {
-        return this.endpoints.addAll(List.of(endpoints));
+        Objects.requireNonNull(endpoints);
+
+        try {
+            lock.writeLock().lock();
+
+            return this.endpoints.addAll(Set.of(endpoints));
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public boolean removeEndpoint(URI... endpoints) {
-        return this.endpoints.removeAll(List.of(endpoints));
+        Objects.requireNonNull(endpoints);
+
+        try {
+            lock.writeLock().lock();
+
+            return this.endpoints.removeAll(Set.of(endpoints));
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 }
