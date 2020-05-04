aefa0d6cc30051fd3b82d4fdb8e80ddb24993405
bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-05-04 13:45:35 +0200

    Resolve "Identität aus Umgebungsvariablen lesen"

--- drasyl-core/src/test/java/org/drasyl/core/node/identity/IdentityManagerTest.java
+++ drasyl-core/src/test/java/org/drasyl/core/node/identity/IdentityManagerTest.java
@@ -19,7 +19,9 @@
 package org.drasyl.core.node.identity;
 
 import com.google.common.io.Files;
-import org.drasyl.core.models.DrasylException;
+import org.drasyl.core.node.DrasylNodeConfig;
+import org.drasyl.crypto.Crypto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -29,37 +31,77 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class IdentityManagerTest {
-    @Test
-    public void loadOrCreateIdentityShouldLoadIdentityIfFileIsPresent(@TempDir Path dir) throws IOException, IdentityManagerException {
-        // create existing file with identity
-        Path path = Paths.get(dir.toString(), "my-identity.json");
-        Files.write("{\n" +
-                "  \"publicKey\" : \"0229041b273dd5ee1c2bef2d77ae17dbd00d2f0a2e939e22d42ef1c4bf05147ea9\",\n" +
-                "  \"privateKey\" : \"0b01459ef93b2b7dc22794a3b9b7e8fac293399cf9add5b2375d9c357a64546d\"\n" +
-                "}", path.toFile(), Charset.defaultCharset());
+    private DrasylNodeConfig config;
 
-        IdentityManager identityManager = new IdentityManager(path);
+    @BeforeEach
+    void setUp() {
+        config = mock(DrasylNodeConfig.class);
+    }
+
+    @Test
+    public void loadOrCreateIdentityShouldLoadValidIdentityFromConfig() throws IdentityManagerException {
+        when(config.getIdentityPublicKey()).thenReturn("0229041b273dd5ee1c2bef2d77ae17dbd00d2f0a2e939e22d42ef1c4bf05147ea9");
+        when(config.getIdentityPrivateKey()).thenReturn("0b01459ef93b2b7dc22794a3b9b7e8fac293399cf9add5b2375d9c357a64546d");
+
+        IdentityManager identityManager = new IdentityManager(config);
         identityManager.loadOrCreateIdentity();
 
         assertEquals(Identity.of("0229041b27"), identityManager.getIdentity());
     }
 
     @Test
-    public void loadOrCreateIdentityShouldThrowExceptionIfPathDoesNotExist(@TempDir Path dir) {
-        Path path = Paths.get(dir.toString(), "non-existing", "my-identity.json");
+    public void loadOrCreateIdentityShouldRejectInvalidIdentityFromConfig() {
+        when(config.getIdentityPublicKey()).thenReturn("this is garbage");
+        when(config.getIdentityPrivateKey()).thenReturn("");
 
-        IdentityManager identityManager = new IdentityManager(path);
+        IdentityManager identityManager = new IdentityManager(config);
 
         assertThrows(IdentityManagerException.class, () -> identityManager.loadOrCreateIdentity());
     }
 
     @Test
-    public void loadOrCreateIdentityShouldCreateNewIdentityIfFileIsAbsent(@TempDir Path dir) throws IdentityManagerException {
+    public void loadOrCreateIdentityShouldLoadIdentityIfConfigContainsNoKeysAndFileIsPresent(@TempDir Path dir) throws IOException, IdentityManagerException {
         Path path = Paths.get(dir.toString(), "my-identity.json");
+        when(config.getIdentityPublicKey()).thenReturn("");
+        when(config.getIdentityPrivateKey()).thenReturn("");
+        when(config.getIdentityPath()).thenReturn(path);
 
-        IdentityManager identityManager = new IdentityManager(path);
+        // create existing file with identity
+        Files.write("{\n" +
+                "  \"publicKey\" : \"0229041b273dd5ee1c2bef2d77ae17dbd00d2f0a2e939e22d42ef1c4bf05147ea9\",\n" +
+                "  \"privateKey\" : \"0b01459ef93b2b7dc22794a3b9b7e8fac293399cf9add5b2375d9c357a64546d\"\n" +
+                "}", path.toFile(), Charset.defaultCharset());
+
+        IdentityManager identityManager = new IdentityManager(config);
+        identityManager.loadOrCreateIdentity();
+
+        assertEquals(Identity.of("0229041b27"), identityManager.getIdentity());
+    }
+
+    @Test
+    public void loadOrCreateIdentityShouldThrowExceptionIfConfigContainsNoKeysAndPathDoesNotExist(@TempDir Path dir) {
+        Path path = Paths.get(dir.toString(), "non-existing", "my-identity.json");
+        when(config.getIdentityPublicKey()).thenReturn("");
+        when(config.getIdentityPrivateKey()).thenReturn("");
+        when(config.getIdentityPath()).thenReturn(path);
+
+        IdentityManager identityManager = new IdentityManager(config);
+
+        assertThrows(IdentityManagerException.class, () -> identityManager.loadOrCreateIdentity());
+    }
+
+    @Test
+    public void loadOrCreateIdentityShouldCreateNewIdentityIfConfigContainsNoKeysAndFileIsAbsent(@TempDir Path dir) throws IdentityManagerException {
+        Path path = Paths.get(dir.toString(), "my-identity.json");
+        when(config.getIdentityPublicKey()).thenReturn("");
+        when(config.getIdentityPrivateKey()).thenReturn("");
+        when(config.getIdentityPath()).thenReturn(path);
+
+        IdentityManager identityManager = new IdentityManager(config);
         identityManager.loadOrCreateIdentity();
 
         assertNotNull(identityManager.getIdentity());
