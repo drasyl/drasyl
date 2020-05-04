8eda534d1a8ca83335d6643b087e831049814e4e
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-05-04 14:16:34 +0200

    User container name according to environment name

--- prepare-environment-docker-compose.sh
+++ prepare-environment-docker-compose.sh
@@ -11,7 +11,7 @@ services:
   $CI_PROJECT_NAME:
     image: $APP_IMAGE
     restart: always
-    container_name: $CI_PROJECT_NAME-$CI_PROJECT_ID-$CI_COMMIT_REF_SLUG
+    container_name: "${CI_PROJECT_NAME}-${CI_PROJECT_ID}-${CI_ENVIRONMENT_SLUG}"
     hostname: ${APP_DEPLOY_HOST}
     environment:
       CONFIG_FORCE_drasyl_identity_public__key: $DRASYL_PUBLIC_KEY
@@ -27,8 +27,8 @@ services:
       - "traefik.enable=true"
       - "traefik.docker.network=proxy_default"
 
-      - "traefik.http.services.${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}.loadbalancer.server.port=22527"
-      - "traefik.http.routers.${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}.rule=Host(\`${APP_DEPLOY_HOST}\`)"
-      - "traefik.http.routers.${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}.entrypoints=websecure"
-      - "traefik.http.routers.${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}.tls.certresolver=mytlschallenge"
+      - "traefik.http.services.${CI_PROJECT_NAME}-${CI_PROJECT_ID}-${CI_ENVIRONMENT_SLUG}.loadbalancer.server.port=22527"
+      - "traefik.http.routers.${CI_PROJECT_NAME}-${CI_PROJECT_ID}-${CI_ENVIRONMENT_SLUG}.rule=Host(\`${APP_DEPLOY_HOST}\`)"
+      - "traefik.http.routers.${CI_PROJECT_NAME}-${CI_PROJECT_ID}-${CI_ENVIRONMENT_SLUG}.entrypoints=websecure"
+      - "traefik.http.routers.${CI_PROJECT_NAME}-${CI_PROJECT_ID}-${CI_ENVIRONMENT_SLUG}.tls.certresolver=mytlschallenge"
 EOF
