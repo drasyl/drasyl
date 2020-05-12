3ecdf2dcf574f1bcb051246b51c427eb8cf65d0c
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-05-13 00:53:10 +0200

    Add some more tests

--- drasyl-core/src/test/java/org/drasyl/core/server/handler/ServerSessionHandlerTest.java
+++ drasyl-core/src/test/java/org/drasyl/core/server/handler/ServerSessionHandlerTest.java
@@ -5,14 +5,13 @@ import io.netty.channel.ServerChannel;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.drasyl.core.common.handler.codec.message.MessageDecoder;
-import org.drasyl.core.common.message.LeaveMessage;
-import org.drasyl.core.common.message.Message;
-import org.drasyl.core.common.message.ResponseMessage;
-import org.drasyl.core.common.message.StatusMessage;
+import org.drasyl.core.common.message.*;
 import org.drasyl.core.common.message.action.MessageAction;
 import org.drasyl.core.common.message.action.ServerMessageAction;
+import org.drasyl.core.models.CompressedPublicKey;
 import org.drasyl.core.node.connections.ClientConnection;
 import org.drasyl.core.server.NodeServer;
+import org.drasyl.crypto.CryptoException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,9 +34,11 @@ class ServerSessionHandlerTest {
     private MessageAction messageAction;
     private Message message;
     private ServerMessageAction serverMessageAction;
+    private JoinMessage joinMessage;
+    private CompressedPublicKey compressedPublicKey;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws CryptoException {
         nodeServer = mock(NodeServer.class);
         uri = URI.create("ws://example.com");
         completableFuture = mock(CompletableFuture.class);
@@ -46,14 +47,19 @@ class ServerSessionHandlerTest {
         messageAction = mock(MessageAction.class);
         message = mock(Message.class);
         serverMessageAction = mock(ServerMessageAction.class);
+        joinMessage = mock(JoinMessage.class);
+        compressedPublicKey = CompressedPublicKey.of("030b6adedef11147b3eea4b4f526f1226ffab218f2b81497e5175e6496f7aa929d");
 
         ChannelHandler handler = new ServerSessionHandler(nodeServer, completableFuture, clientConnection, uri);
         channel = new EmbeddedChannel(handler);
     }
 
     @Test
-    void shouldSetResponseForResponseMessageIfSessionIsCreated() {
+    void shouldSetResponseForResponseMessageIfSessionExists() {
         when(responseMessage.getAction()).thenReturn(messageAction);
+
+        ChannelHandler handler = new ServerSessionHandler(nodeServer, completableFuture, clientConnection, uri);
+        channel = new EmbeddedChannel(handler);
 
         channel.writeInbound(responseMessage);
         channel.flush();
@@ -62,12 +68,39 @@ class ServerSessionHandlerTest {
     }
 
     @Test
-    void shouldExecuteOnServerActionForMessageIfSessionIsCreated() {
+    void shouldExecuteOnServerActionForMessageIfSessionExists() {
         when(message.getAction()).thenReturn(serverMessageAction);
+
+        ChannelHandler handler = new ServerSessionHandler(nodeServer, completableFuture, clientConnection, uri);
+        channel = new EmbeddedChannel(handler);
 
         channel.writeInbound(message);
         channel.flush();
 
         verify(serverMessageAction).onMessageServer(clientConnection, nodeServer);
+    }
+
+    @Test
+    void shouldCreateSessionOnJoinMessageIfNoSessionExists() {
+        when(joinMessage.getPublicKey()).thenReturn(compressedPublicKey);
+
+        ChannelHandler handler = new ServerSessionHandler(nodeServer, completableFuture, null, uri);
+        channel = new EmbeddedChannel(handler);
+
+        channel.writeInbound(joinMessage);
+        channel.flush();
+
+        verify(completableFuture).complete(any());
+    }
+
+    @Test
+    void shouldCreateNoSessionOnJoinMessageIfSessionExists() {
+        ChannelHandler handler = new ServerSessionHandler(nodeServer, completableFuture, clientConnection, uri);
+        channel = new EmbeddedChannel(handler);
+
+        channel.writeInbound(joinMessage);
+        channel.flush();
+
+        verify(completableFuture, never()).complete(any());
     }
 }
