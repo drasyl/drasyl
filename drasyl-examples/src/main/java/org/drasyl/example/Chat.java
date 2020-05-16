eee8679389ffe8336e0fad32b8d6a5a900417021
bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-05-16 13:52:03 +0200

    Examples

---
+++ drasyl-examples/src/main/java/org/drasyl/example/Chat.java
@@ -0,0 +1,79 @@
+package org.drasyl.example;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.drasyl.core.common.models.Pair;
+import org.drasyl.core.models.DrasylException;
+import org.drasyl.core.models.Event;
+import org.drasyl.core.node.DrasylNode;
+import org.drasyl.core.node.identity.Identity;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * This is an Example of a Chat Application running on the drasyl Overlay Network. It allows you to
+ * send Text Messages to other drasyl Nodes running this Chat Application.
+ */
+public class Chat {
+    public static void main(String[] args) throws DrasylException, InterruptedException {
+        Config config;
+        if (args.length == 1) {
+            config = ConfigFactory.parseFile(new File(args[0])).withFallback(ConfigFactory.load());
+        }
+        else {
+            config = ConfigFactory.load();
+        }
+
+        CompletableFuture<Void> online = new CompletableFuture<>();
+        DrasylNode node = new DrasylNode(config) {
+            @Override
+            public void onEvent(Event event) {
+                switch (event.getCode()) {
+                    case MESSAGE:
+                        Pair<Identity, byte[]> message = event.getMessage();
+                        System.out.println("From " + message.first().getId() + ": " + new String(message.second()));
+                        break;
+                    case NODE_ONLINE:
+                        online.complete(null);
+                        System.out.println("Your Address is: " + event.getNode().getAddress().getId());
+                        break;
+                    default:
+                        System.out.println(event);
+                }
+            }
+        };
+        node.start().join();
+        online.join();
+
+        System.out.println();
+        System.out.println("****************************************************************************************");
+        System.out.println("This is an Example of a Chat Application running on the drasyl Overlay Network.");
+        System.out.println("It allows you to send Text Messages to other drasyl Nodes running this Chat Application.");
+        System.out.println("****************************************************************************************");
+        System.out.println();
+
+        String recipient = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                // prompt for recipient
+                System.out.print("Recipient [" + recipient + "]? ");
+                recipient = scanner.nextLine();
+
+                // prompt for message
+                System.out.print("Message? ");
+                String message = scanner.nextLine();
+
+                node.send(recipient, message);
+                System.out.println("To " + recipient + ": " + message);
+            }
+            catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+
+            Thread.sleep(1000);
+        }
+    }
+}
