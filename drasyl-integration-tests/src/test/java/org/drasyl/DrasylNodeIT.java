22f7bdba2dd663f707cee65015782521354041ea
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-06-06 11:28:26 +0200

    Enhance DrasylNodeIT

--- drasyl-integration-tests/src/test/java/org/drasyl/DrasylNodeIT.java
+++ drasyl-integration-tests/src/test/java/org/drasyl/DrasylNodeIT.java
@@ -29,6 +29,7 @@ import org.drasyl.event.EventType;
 import org.drasyl.util.Pair;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.Timeout;
@@ -61,76 +62,6 @@ class DrasylNodeIT {
         colorizedPrintln("FINISHED " + info.getDisplayName(), COLOR_CYAN, STYLE_REVERSED);
     }
 
-    /**
-     * This test ensures that sent application messages are delivered to the recipient. Two clients,
-     * one Super Peer and one Super Super Peer, will be created for this purpose. All nodes send
-     * messages to every other node (including themselves). At the end, a check is made to ensure
-     * that all nodes have received all messages.
-     */
-    @Test
-    @Timeout(value = TIMEOUT, unit = MILLISECONDS)
-    void applicationMessagesShouldBeDelivered() throws DrasylException {
-        //
-        // create nodes
-        //
-        Config config;
-
-        // super super peer
-        config = ConfigFactory.parseString("drasyl.server.bind-port = 22528\ndrasyl.super-peer.enabled = false").withFallback(ConfigFactory.load("configs/DrasylNodeIT-4c4fdd0957.conf"));
-        Pair<DrasylNode, Observable<Event>> superSuperPeer = createNode(config);
-        superSuperPeer.second().filter(e -> e.getType() == EVENT_NODE_UP).test().awaitCount(1);
-        colorizedPrintln("CREATED superSuperPeer", COLOR_CYAN, STYLE_REVERSED);
-
-        // super peer
-        config = ConfigFactory.parseString("drasyl.server.bind-port = 22529\ndrasyl.super-peer.public-key = \"03409386a22294ee55393eb0f83483c54f847f700df687668cc8aa3caa19a9df7a\"\ndrasyl.super-peer.endpoints = [\"ws://127.0.0.1:22528\"]").withFallback(ConfigFactory.load("configs/DrasylNodeIT-9df9214d78.conf"));
-        Pair<DrasylNode, Observable<Event>> superPeer = createNode(config);
-        superPeer.second().filter(e -> e.getType() == EVENT_NODE_ONLINE).test().awaitCount(1);
-        colorizedPrintln("CREATED superPeer", COLOR_CYAN, STYLE_REVERSED);
-
-        // client1
-        config = ConfigFactory.parseString("drasyl.server.enabled = false\ndrasyl.super-peer.public-key = \"030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22\"\ndrasyl.super-peer.endpoints = [\"ws://127.0.0.1:22529\"]").withFallback(ConfigFactory.load("configs/DrasylNodeIT-030f018704.conf"));
-        Pair<DrasylNode, Observable<Event>> client1 = createNode(config);
-        client1.second().filter(e -> e.getType() == EVENT_NODE_ONLINE).test().awaitCount(1);
-        colorizedPrintln("CREATED client1", COLOR_CYAN, STYLE_REVERSED);
-
-        // client2
-        config = ConfigFactory.parseString("drasyl.server.enabled = false\ndrasyl.super-peer.public-key = \"030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22\"\ndrasyl.super-peer.endpoints = [\"ws://127.0.0.1:22529\"]").withFallback(ConfigFactory.load("configs/DrasylNodeIT-be0300f1a4.conf"));
-        Pair<DrasylNode, Observable<Event>> client2 = createNode(config);
-        client2.second().filter(e -> e.getType() == EVENT_NODE_ONLINE).test().awaitCount(1);
-        colorizedPrintln("CREATED client2", COLOR_CYAN, STYLE_REVERSED);
-
-        //
-        // send messages
-        //
-        TestObserver<EventType> superSuperPeerMessages = superSuperPeer.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
-        TestObserver<EventType> superPeerMessages = superPeer.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
-        TestObserver<EventType> client1Messages = client1.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
-        TestObserver<EventType> client2Messages = client2.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
-
-//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SSP: " + e));
-//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SP: " + e));
-//        client1.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C1: " + e));
-//        client2.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C2: " + e));
-
-        Set<String> identities = Set.of("4c4fdd0957", "9df9214d78", "030f018704", "be0300f1a4");
-        for (String recipient : identities) {
-            if (!recipient.equals("030f018704") && !recipient.equals("be0300f1a4")) { // TODO: sending to grandchildren is not yet supported
-                superSuperPeer.first().send(recipient, "Hallo Welt");
-            }
-            superPeer.first().send(recipient, "Hallo Welt");
-            client1.first().send(recipient, "Hallo Welt");
-            client2.first().send(recipient, "Hallo Welt");
-        }
-
-        //
-        // verify
-        //
-        superSuperPeerMessages.awaitCount(4);
-        superPeerMessages.awaitCount(4);
-        client1Messages.awaitCount(3); // TODO: sending to grandchildren is not yet supported
-        client2Messages.awaitCount(3); // TODO: sending to grandchildren is not yet supported
-    }
-
     private Pair<DrasylNode, Observable<Event>> createNode(Config config) throws DrasylException {
         Subject<Event> subject = ReplaySubject.create();
         DrasylNode node = new DrasylNode(config) {
@@ -144,5 +75,181 @@ class DrasylNodeIT {
         nodes.add(node);
 
         return Pair.of(node, subject);
+    }
+
+    /**
+     * Two clients, one Super Peer and one Super Super Peer will be created for this test.
+     */
+    @Nested
+    class WhenNothingIsDisabled {
+        private Pair<DrasylNode, Observable<Event>> superSuperPeer;
+        private Pair<DrasylNode, Observable<Event>> superPeer;
+        private Pair<DrasylNode, Observable<Event>> client1;
+        private Pair<DrasylNode, Observable<Event>> client2;
+
+        @BeforeEach
+        void setUp() throws DrasylException {
+            //
+            // create nodes
+            //
+            Config config;
+
+            // super super peer
+            config = ConfigFactory.parseString("drasyl.server.bind-port = 22528\n" +
+                    "drasyl.super-peer.enabled = false")
+                    .withFallback(ConfigFactory.load("configs/DrasylNodeIT-4c4fdd0957.conf"));
+            superSuperPeer = createNode(config);
+            superSuperPeer.second().filter(e -> e.getType() == EVENT_NODE_UP).test().awaitCount(1);
+            colorizedPrintln("CREATED superSuperPeer", COLOR_CYAN, STYLE_REVERSED);
+
+            // super peer
+            config = ConfigFactory.parseString("drasyl.server.bind-port = 22529\n" +
+                    "drasyl.super-peer.public-key = \"03409386a22294ee55393eb0f83483c54f847f700df687668cc8aa3caa19a9df7a\"\n" +
+                    "drasyl.super-peer.endpoints = [\"ws://127.0.0.1:22528\"]")
+                    .withFallback(ConfigFactory.load("configs/DrasylNodeIT-9df9214d78.conf"));
+            superPeer = createNode(config);
+            superPeer.second().filter(e -> e.getType() == EVENT_NODE_ONLINE).test().awaitCount(1);
+            colorizedPrintln("CREATED superPeer", COLOR_CYAN, STYLE_REVERSED);
+
+            // client1
+            config = ConfigFactory.parseString("drasyl.server.enabled = false\n" +
+                    "drasyl.super-peer.public-key = \"030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22\"\n" +
+                    "drasyl.super-peer.endpoints = [\"ws://127.0.0.1:22529\"]").withFallback(ConfigFactory.load("configs/DrasylNodeIT-030f018704.conf"));
+            client1 = createNode(config);
+            client1.second().filter(e -> e.getType() == EVENT_NODE_ONLINE).test().awaitCount(1);
+            colorizedPrintln("CREATED client1", COLOR_CYAN, STYLE_REVERSED);
+
+            // client2
+            config = ConfigFactory.parseString("drasyl.server.enabled = false\n" +
+                    "drasyl.super-peer.public-key = \"030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22\"\n" +
+                    "drasyl.super-peer.endpoints = [\"ws://127.0.0.1:22529\"]").withFallback(ConfigFactory.load("configs/DrasylNodeIT-be0300f1a4.conf"));
+            client2 = createNode(config);
+            client2.second().filter(e -> e.getType() == EVENT_NODE_ONLINE).test().awaitCount(1);
+            colorizedPrintln("CREATED client2", COLOR_CYAN, STYLE_REVERSED);
+        }
+
+        /**
+         * This test ensures that sent application messages are delivered to the recipient. All
+         * nodes send messages to every other node (including themselves). At the end, a check is
+         * made to ensure that all nodes have received all messages.
+         */
+        @Test
+        @Timeout(value = TIMEOUT, unit = MILLISECONDS)
+        void applicationMessagesShouldBeDelivered() throws DrasylException {
+            //
+            // send messages
+            //
+            TestObserver<EventType> superSuperPeerMessages = superSuperPeer.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+            TestObserver<EventType> superPeerMessages = superPeer.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+            TestObserver<EventType> client1Messages = client1.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+            TestObserver<EventType> client2Messages = client2.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+
+//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SSP: " + e));
+//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SP: " + e));
+//        client1.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C1: " + e));
+//        client2.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C2: " + e));
+
+            Set<String> identities = Set.of("4c4fdd0957", "9df9214d78", "030f018704", "be0300f1a4");
+            for (String recipient : identities) {
+                if (!recipient.equals("030f018704") && !recipient.equals("be0300f1a4")) { // TODO: sending to grandchildren is not yet supported
+                    superSuperPeer.first().send(recipient, "Hallo Welt");
+                }
+                superPeer.first().send(recipient, "Hallo Welt");
+                client1.first().send(recipient, "Hallo Welt");
+                client2.first().send(recipient, "Hallo Welt");
+            }
+
+            //
+            // verify
+            //
+            superSuperPeerMessages.awaitCount(4);
+            superPeerMessages.awaitCount(4);
+            client1Messages.awaitCount(3); // TODO: sending to grandchildren is not yet supported
+            client2Messages.awaitCount(3); // TODO: sending to grandchildren is not yet supported
+        }
+    }
+
+    /**
+     * Four nodes with disabled sever and disabled super peer will be created for this test.
+     */
+    @Nested
+    class WhenServerAndSuperPeerAreDisabled {
+        private Pair<DrasylNode, Observable<Event>> node1;
+        private Pair<DrasylNode, Observable<Event>> node2;
+        private Pair<DrasylNode, Observable<Event>> node3;
+        private Pair<DrasylNode, Observable<Event>> node4;
+
+        @BeforeEach
+        void setUp() throws DrasylException {
+            //
+            // create nodes
+            //
+            Config config;
+
+            // super super peer
+            config = ConfigFactory.parseString("drasyl.server.enabled = false\n" +
+                    "drasyl.super-peer.enabled = false")
+                    .withFallback(ConfigFactory.load("configs/DrasylNodeIT-4c4fdd0957.conf"));
+            node1 = createNode(config);
+            node1.second().filter(e -> e.getType() == EVENT_NODE_UP).test().awaitCount(1);
+            colorizedPrintln("CREATED node1", COLOR_CYAN, STYLE_REVERSED);
+
+            // super peer
+            config = ConfigFactory.parseString("drasyl.server.enabled = false\n" +
+                    "drasyl.super-peer.enabled = false")
+                    .withFallback(ConfigFactory.load("configs/DrasylNodeIT-9df9214d78.conf"));
+            node2 = createNode(config);
+            node2.second().filter(e -> e.getType() == EVENT_NODE_UP).test().awaitCount(1);
+            colorizedPrintln("CREATED node2", COLOR_CYAN, STYLE_REVERSED);
+
+            // client1
+            config = ConfigFactory.parseString("drasyl.server.enabled = false\n" +
+                    "drasyl.super-peer.enabled = false").withFallback(ConfigFactory.load("configs/DrasylNodeIT-030f018704.conf"));
+            node3 = createNode(config);
+            node3.second().filter(e -> e.getType() == EVENT_NODE_UP).test().awaitCount(1);
+            colorizedPrintln("CREATED node3", COLOR_CYAN, STYLE_REVERSED);
+
+            // client2
+            config = ConfigFactory.parseString("drasyl.server.enabled = false\n" +
+                    "drasyl.super-peer.enabled = false").withFallback(ConfigFactory.load("configs/DrasylNodeIT-be0300f1a4.conf"));
+            node4 = createNode(config);
+            node4.second().filter(e -> e.getType() == EVENT_NODE_UP).test().awaitCount(1);
+            colorizedPrintln("CREATED node4", COLOR_CYAN, STYLE_REVERSED);
+        }
+
+        /**
+         * This test ensures that loopback message discovery work. Every node an application
+         * messages to itself. At the end, a check is made to ensure that all nodes have received a
+         * message.
+         */
+        @Test
+        @Timeout(value = TIMEOUT, unit = MILLISECONDS)
+        void applicationMessagesShouldBeDelivered() throws DrasylException {
+            //
+            // send messages
+            //
+            TestObserver<EventType> superSuperPeerMessages = node1.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+            TestObserver<EventType> superPeerMessages = node2.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+            TestObserver<EventType> client1Messages = node3.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+            TestObserver<EventType> client2Messages = node4.second().map(e -> e.getType()).filter(c -> c == EVENT_MESSAGE).test();
+
+//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SSP: " + e));
+//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SP: " + e));
+//        client1.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C1: " + e));
+//        client2.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C2: " + e));
+
+            node1.first().send("4c4fdd0957", "Hallo Welt");
+            node2.first().send("9df9214d78", "Hallo Welt");
+            node3.first().send("030f018704", "Hallo Welt");
+            node4.first().send("be0300f1a4", "Hallo Welt");
+
+            //
+            // verify
+            //
+            superSuperPeerMessages.awaitCount(1);
+            superPeerMessages.awaitCount(1);
+            client1Messages.awaitCount(1);
+            client2Messages.awaitCount(1);
+        }
     }
 }
