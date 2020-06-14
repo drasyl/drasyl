b73a0b2e5677c414b8727c3c79e781d0f8a55863
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-06-14 13:34:16 +0200

    add information at this stage to avoid identity collision at session creation stage

--- drasyl-core/src/main/java/org/drasyl/peer/connection/server/handler/NodeServerConnectionHandler.java
+++ drasyl-core/src/main/java/org/drasyl/peer/connection/server/handler/NodeServerConnectionHandler.java
@@ -131,6 +131,10 @@ public class NodeServerConnectionHandler extends AbstractThreeWayHandshakeServer
     @Override
     protected ConnectionExceptionMessage.Error validateSessionRequest(JoinMessage requestMessage) {
         Identity clientIdentity = requestMessage.getIdentity();
+        PeerInformation clientInformation = requestMessage.getPeerInformation();
+
+        // add information at this stage to avoid identity collision at session creation stage
+        peersManager.addPeerInformation(clientIdentity, clientInformation);
 
         if (this.ownIdentity.equals(clientIdentity)) {
             return CONNECTION_ERROR_IDENTITY_COLLISION;
