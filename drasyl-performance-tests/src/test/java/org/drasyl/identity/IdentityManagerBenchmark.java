4f4dfb1c55ddfbb445778db728ab24780700d069
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-06-15 18:14:32 +0200

    Benchmark for IdentityManager.generateIdentity() added

---
+++ drasyl-performance-tests/src/test/java/org/drasyl/identity/IdentityManagerBenchmark.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020.
+ *
+ * This file is part of drasyl.
+ *
+ *  drasyl is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  drasyl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with drasyl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.drasyl.identity;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+//@Fork(value = 1)
+//@Warmup(iterations = 1)
+//@Measurement(iterations = 1)
+public class IdentityManagerBenchmark {
+    @Benchmark
+    public void generateIdentity() throws IdentityManagerException {
+        IdentityManager.generateIdentity();
+    }
+}
