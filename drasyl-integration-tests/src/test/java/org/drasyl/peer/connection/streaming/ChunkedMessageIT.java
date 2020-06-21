aa47f70882c520753f867a2d65403c5f5f3a272a
Kevin Röbert <dev@roebert.eu>
2020-06-21 23:25:47 +0200

    it

--- drasyl-integration-tests/src/test/java/org/drasyl/peer/connection/streaming/ChunkedMessageIT.java
+++ drasyl-integration-tests/src/test/java/org/drasyl/peer/connection/streaming/ChunkedMessageIT.java
@@ -18,7 +18,6 @@
  */
 package org.drasyl.peer.connection.streaming;
 
-import com.typesafe.config.ConfigFactory;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.reactivex.rxjava3.core.Observable;
@@ -26,9 +25,12 @@ import org.drasyl.DrasylException;
 import org.drasyl.DrasylNode;
 import org.drasyl.DrasylNodeConfig;
 import org.drasyl.crypto.CryptoException;
+import org.drasyl.identity.CompressedPrivateKey;
+import org.drasyl.identity.CompressedPublicKey;
 import org.drasyl.identity.Identity;
 import org.drasyl.identity.IdentityManager;
 import org.drasyl.identity.IdentityManagerException;
+import org.drasyl.identity.ProofOfWork;
 import org.drasyl.messenger.Messenger;
 import org.drasyl.peer.PeersManager;
 import org.drasyl.peer.connection.message.ApplicationMessage;
@@ -48,11 +50,14 @@ import testutils.AnsiColor;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
+import static java.time.Duration.ofSeconds;
 import static org.drasyl.peer.connection.server.TestNodeServerConnection.clientSessionAfterJoin;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static testutils.AnsiColor.COLOR_CYAN;
+import static testutils.AnsiColor.STYLE_REVERSED;
 import static testutils.TestHelper.colorizedPrintln;
 
-@Disabled("Only for benchmark purposes")
+
 class ChunkedMessageIT {
     private static EventLoopGroup workerGroup;
     private static EventLoopGroup bossGroup;
@@ -63,15 +68,27 @@ class ChunkedMessageIT {
 
     @BeforeEach
     void setup(TestInfo info) throws DrasylException, CryptoException {
-        colorizedPrintln("STARTING " + info.getDisplayName(), AnsiColor.COLOR_CYAN, AnsiColor.STYLE_REVERSED);
+        colorizedPrintln("STARTING " + info.getDisplayName(), COLOR_CYAN, STYLE_REVERSED);
 
         System.setProperty("io.netty.tryReflectionSetAccessible", "true");
-        System.setProperty("io.netty.leakDetection.level", "DISABLED"); //DISABLED
+        System.setProperty("io.netty.leakDetection.level", "DISABLED");
 
         identitySession1 = Identity.of(169092, "030a59784f88c74dcd64258387f9126739c3aeb7965f36bb501ff01f5036b3d72b", "0f1e188d5e3b98daf2266d7916d2e1179ae6209faa7477a2a66d4bb61dab4399");
         identitySession2 = Identity.of(26778671, "0236fde6a49564a0eaa2a7d6c8f73b97062d5feb36160398c08a5b73f646aa5fe5", "093d1ee70518508cac18eaf90d312f768c14d43de9bfd2618a2794d8df392da0");
 
-        config = new DrasylNodeConfig(ConfigFactory.load("configs/ChunkedMessageIT.conf"));
+        config = DrasylNodeConfig.newBuilder()
+                .messageMaxContentLength(102400000)
+                .identityProofOfWork(ProofOfWork.of(6657650))
+                .identityPublicKey(CompressedPublicKey.of("023d34f317616c3bb0fa1e4b425e9419d1704ef57f6e53afe9790e00998134f5ff"))
+                .identityPrivateKey(CompressedPrivateKey.of("0c27af38c77f2cd5cc2a0ff5c461003a9c24beb955f316135d251ecaf4dda03f"))
+                .serverBindHost("127.0.0.1")
+                .serverBindPort(0)
+                .serverHandshakeTimeout(ofSeconds(5))
+                .serverSSLEnabled(true)
+                .serverIdleTimeout(ofSeconds(1))
+                .serverIdleRetries((short) 1)
+                .superPeerEnabled(false)
+                .build();
         DrasylNode.setLogLevel(config.getLoglevel());
         IdentityManager identityManager = new IdentityManager(config);
         identityManager.loadOrCreateIdentity();
@@ -80,7 +97,7 @@ class ChunkedMessageIT {
         Messenger messenger = new Messenger();
         Observable<Boolean> superPeerConnected = Observable.just(false);
 
-        server = new NodeServer(identityManager, messenger, peersManager, superPeerConnected, config, workerGroup, bossGroup);
+        server = new NodeServer(identityManager::getIdentity, messenger, peersManager, config, workerGroup, bossGroup, superPeerConnected);
         server.open();
     }
 
