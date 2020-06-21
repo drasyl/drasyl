e03dadae67fc9257d10694eff262404cad143042
Kevin Röbert <dev@roebert.eu>
2020-06-22 00:21:45 +0200

    refactor it

--- drasyl-integration-tests/src/test/java/org/drasyl/peer/connection/streaming/ChunkedMessageIT.java
+++ drasyl-integration-tests/src/test/java/org/drasyl/peer/connection/streaming/ChunkedMessageIT.java
@@ -21,7 +21,6 @@ package org.drasyl.peer.connection.streaming;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.reactivex.rxjava3.core.Observable;
-import org.drasyl.DrasylException;
 import org.drasyl.DrasylNode;
 import org.drasyl.DrasylNodeConfig;
 import org.drasyl.crypto.CryptoException;
@@ -37,13 +36,14 @@ import org.drasyl.peer.connection.message.ApplicationMessage;
 import org.drasyl.peer.connection.message.Message;
 import org.drasyl.peer.connection.message.RequestMessage;
 import org.drasyl.peer.connection.server.NodeServer;
+import org.drasyl.peer.connection.server.NodeServerException;
 import org.drasyl.peer.connection.server.TestNodeServerConnection;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.TestInfo;
 import testutils.AnsiColor;
 
@@ -61,23 +61,47 @@ import static testutils.TestHelper.colorizedPrintln;
 class ChunkedMessageIT {
     private static EventLoopGroup workerGroup;
     private static EventLoopGroup bossGroup;
-    DrasylNodeConfig config;
-    private NodeServer server;
-    private Identity identitySession1;
-    private Identity identitySession2;
+    private static DrasylNodeConfig config;
+    private static NodeServer server;
+    private static TestNodeServerConnection session1;
+    private static TestNodeServerConnection session2;
+    private static byte[] bigPayload;
 
     @BeforeEach
-    void setup(TestInfo info) throws DrasylException, CryptoException {
+    void setup(TestInfo info) {
         colorizedPrintln("STARTING " + info.getDisplayName(), COLOR_CYAN, STYLE_REVERSED);
+    }
+
+    @AfterEach
+    void cleanUp(TestInfo info) {
+        colorizedPrintln("FINISHED " + info.getDisplayName(), AnsiColor.COLOR_CYAN, AnsiColor.STYLE_REVERSED);
+    }
+
+    @RepeatedTest(20)
+    void messageWithMaxSizeShouldArrive() {
+        Observable<Message> receivedMessages = session2.receivedMessages().filter(msg -> msg instanceof ApplicationMessage);
+
+        // send message
+        RequestMessage request = new ApplicationMessage(session1.getPublicKey(), session2.getPublicKey(), bigPayload);
+        session2.send(request);
+
+        // verify response
+        assertEquals(request, receivedMessages.blockingFirst());
+    }
+
+    @BeforeAll
+    static void beforeAll() throws IdentityManagerException, NodeServerException, CryptoException, ExecutionException, InterruptedException {
+        workerGroup = new NioEventLoopGroup();
+        bossGroup = new NioEventLoopGroup(1);
 
         System.setProperty("io.netty.tryReflectionSetAccessible", "true");
         System.setProperty("io.netty.leakDetection.level", "DISABLED");
 
-        identitySession1 = Identity.of(169092, "030a59784f88c74dcd64258387f9126739c3aeb7965f36bb501ff01f5036b3d72b", "0f1e188d5e3b98daf2266d7916d2e1179ae6209faa7477a2a66d4bb61dab4399");
-        identitySession2 = Identity.of(26778671, "0236fde6a49564a0eaa2a7d6c8f73b97062d5feb36160398c08a5b73f646aa5fe5", "093d1ee70518508cac18eaf90d312f768c14d43de9bfd2618a2794d8df392da0");
+        Identity identitySession1 = Identity.of(169092, "030a59784f88c74dcd64258387f9126739c3aeb7965f36bb501ff01f5036b3d72b", "0f1e188d5e3b98daf2266d7916d2e1179ae6209faa7477a2a66d4bb61dab4399");
+        Identity identitySession2 = Identity.of(26778671, "0236fde6a49564a0eaa2a7d6c8f73b97062d5feb36160398c08a5b73f646aa5fe5", "093d1ee70518508cac18eaf90d312f768c14d43de9bfd2618a2794d8df392da0");
 
         config = DrasylNodeConfig.newBuilder()
-                .messageMaxContentLength(102400000)
+                .messageMaxContentLength(1024000000)
                 .identityProofOfWork(ProofOfWork.of(6657650))
                 .identityPublicKey(CompressedPublicKey.of("023d34f317616c3bb0fa1e4b425e9419d1704ef57f6e53afe9790e00998134f5ff"))
                 .identityPrivateKey(CompressedPrivateKey.of("0c27af38c77f2cd5cc2a0ff5c461003a9c24beb955f316135d251ecaf4dda03f"))
@@ -99,45 +123,21 @@ class ChunkedMessageIT {
 
         server = new NodeServer(identityManager::getIdentity, messenger, peersManager, config, workerGroup, bossGroup, superPeerConnected);
         server.open();
+
+        session1 = clientSessionAfterJoin(config, server, identitySession1);
+        session2 = clientSessionAfterJoin(config, server, identitySession2);
+
+        // create message with max allowed payload size
+        bigPayload = new byte[config.getMessageMaxContentLength()];
+        new Random().nextBytes(bigPayload);
     }
 
-    @AfterEach
-    void cleanUp(TestInfo info) throws IdentityManagerException {
+    @AfterAll
+    static void afterAll() throws IdentityManagerException {
         server.close();
 
         IdentityManager.deleteIdentityFile(config.getIdentityPath());
 
-        colorizedPrintln("FINISHED " + info.getDisplayName(), AnsiColor.COLOR_CYAN, AnsiColor.STYLE_REVERSED);
-    }
-
-    @Test
-    void messageWithMaxSizeShouldArrive() throws InterruptedException, ExecutionException {
-        // create connection
-        TestNodeServerConnection session1 = clientSessionAfterJoin(config, server, identitySession1);
-        TestNodeServerConnection session2 = clientSessionAfterJoin(config, server, identitySession2);
-
-        Observable<Message> receivedMessages = session2.receivedMessages().filter(msg -> msg instanceof ApplicationMessage);
-
-        // create message with max allowed payload size
-        byte[] bigPayload = new byte[config.getMessageMaxContentLength()];
-        new Random().nextBytes(bigPayload);
-
-        // send message
-        RequestMessage request = new ApplicationMessage(session1.getPublicKey(), session2.getPublicKey(), bigPayload);
-        session2.send(request);
-
-        // verify response
-        assertEquals(request, receivedMessages.blockingFirst());
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        workerGroup = new NioEventLoopGroup();
-        bossGroup = new NioEventLoopGroup(1);
-    }
-
-    @AfterAll
-    static void afterAll() {
         workerGroup.shutdownGracefully().syncUninterruptibly();
         bossGroup.shutdownGracefully().syncUninterruptibly();
     }
