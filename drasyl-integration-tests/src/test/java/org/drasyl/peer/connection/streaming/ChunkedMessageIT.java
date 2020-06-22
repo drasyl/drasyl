a37b08e08a958163f94798faa153defcb28924ed
Kevin Röbert <dev@roebert.eu>
2020-06-23 01:31:00 +0200

    fix test

--- drasyl-integration-tests/src/test/java/org/drasyl/peer/connection/streaming/ChunkedMessageIT.java
+++ drasyl-integration-tests/src/test/java/org/drasyl/peer/connection/streaming/ChunkedMessageIT.java
@@ -24,7 +24,12 @@ import io.reactivex.rxjava3.core.Observable;
 import org.drasyl.DrasylConfig;
 import org.drasyl.DrasylNode;
 import org.drasyl.crypto.CryptoException;
-import org.drasyl.identity.*;
+import org.drasyl.identity.CompressedPrivateKey;
+import org.drasyl.identity.CompressedPublicKey;
+import org.drasyl.identity.Identity;
+import org.drasyl.identity.IdentityManager;
+import org.drasyl.identity.IdentityManagerException;
+import org.drasyl.identity.ProofOfWork;
 import org.drasyl.messenger.Messenger;
 import org.drasyl.peer.PeersManager;
 import org.drasyl.peer.connection.message.ApplicationMessage;
@@ -33,10 +38,17 @@ import org.drasyl.peer.connection.message.RequestMessage;
 import org.drasyl.peer.connection.server.NodeServer;
 import org.drasyl.peer.connection.server.NodeServerException;
 import org.drasyl.peer.connection.server.TestNodeServerConnection;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInfo;
 import testutils.AnsiColor;
 
+import java.net.URI;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import static java.time.Duration.ofSeconds;
@@ -89,35 +101,45 @@ class ChunkedMessageIT {
         Identity identitySession1 = Identity.of(169092, "030a59784f88c74dcd64258387f9126739c3aeb7965f36bb501ff01f5036b3d72b", "0f1e188d5e3b98daf2266d7916d2e1179ae6209faa7477a2a66d4bb61dab4399");
         Identity identitySession2 = Identity.of(26778671, "0236fde6a49564a0eaa2a7d6c8f73b97062d5feb36160398c08a5b73f646aa5fe5", "093d1ee70518508cac18eaf90d312f768c14d43de9bfd2618a2794d8df392da0");
 
-        config = DrasylConfig.newBuilder()
+        DrasylConfig serverConfig = DrasylConfig.newBuilder()
+//                .loglevel(Level.TRACE)
                 .messageMaxContentLength(1024 * 1024 * 100)
                 .identityProofOfWork(ProofOfWork.of(6657650))
                 .identityPublicKey(CompressedPublicKey.of("023d34f317616c3bb0fa1e4b425e9419d1704ef57f6e53afe9790e00998134f5ff"))
                 .identityPrivateKey(CompressedPrivateKey.of("0c27af38c77f2cd5cc2a0ff5c461003a9c24beb955f316135d251ecaf4dda03f"))
                 .serverBindHost("127.0.0.1")
                 .serverBindPort(0)
+                .serverEndpoints(Set.of(URI.create("wss://127.0.0.1:0")))
                 .serverHandshakeTimeout(ofSeconds(5))
                 .serverSSLEnabled(true)
                 .serverIdleTimeout(ofSeconds(1))
                 .serverIdleRetries((short) 1)
                 .superPeerEnabled(false)
                 .build();
-        DrasylNode.setLogLevel(config.getLoglevel());
-        IdentityManager identityManager = new IdentityManager(config);
-        identityManager.loadOrCreateIdentity();
+        DrasylNode.setLogLevel(serverConfig.getLoglevel());
+        IdentityManager serverIdentityManager = new IdentityManager(serverConfig);
+        serverIdentityManager.loadOrCreateIdentity();
         PeersManager peersManager = new PeersManager(event -> {
         });
-        Messenger messenger = new Messenger();
-        Observable<Boolean> superPeerConnected = Observable.just(false);
+        Messenger serverMessenger = new Messenger();
+        Observable<Boolean> serverSuperPeerConnected = Observable.just(false);
 
-        server = new NodeServer(identityManager::getIdentity, messenger, peersManager, config, workerGroup, bossGroup, superPeerConnected);
+        server = new NodeServer(serverIdentityManager::getIdentity, serverMessenger, peersManager, serverConfig, workerGroup, bossGroup, serverSuperPeerConnected);
         server.open();
+
+        config = DrasylConfig.newBuilder()
+//                .loglevel(Level.TRACE)
+                .messageMaxContentLength(1024 * 1024 * 100)
+                .serverEnabled(false)
+                .serverSSLEnabled(true)
+                .superPeerEndpoints(server.getEndpoints())
+                .build();
 
         session1 = clientSessionAfterJoin(config, server, identitySession1);
         session2 = clientSessionAfterJoin(config, server, identitySession2);
 
         // create message with max allowed payload size
-        bigPayload = new byte[config.getMessageMaxContentLength()];
+        bigPayload = new byte[serverConfig.getMessageMaxContentLength()];
         new Random().nextBytes(bigPayload);
     }
 
