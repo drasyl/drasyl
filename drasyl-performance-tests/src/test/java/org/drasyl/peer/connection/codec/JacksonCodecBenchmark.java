879b538b830ce81f287f7f539fe4ccc95902f7f0
Kevin Röbert <dev@roebert.eu>
2020-06-22 02:43:44 +0200

    jacksoncodec
    Liefert ein ByteBuf, der dann allerdings nicht in einem richtigen WebSocketFrame ist und damit die Pipeline sprengt

---
+++ drasyl-performance-tests/src/test/java/org/drasyl/peer/connection/codec/JacksonCodecBenchmark.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020.
+ *
+ * This file is part of drasyl.
+ *
+ *  drasyl is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  drasyl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with drasyl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.drasyl.peer.connection.codec;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import org.drasyl.identity.CompressedPublicKey;
+import org.drasyl.peer.connection.message.ApplicationMessage;
+import org.drasyl.peer.connection.message.Message;
+import org.mockito.Answers;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.drasyl.util.JSONUtil.JACKSON_WRITER;
+import static org.mockito.Mockito.mock;
+
+@State(Scope.Benchmark)
+//@Fork(value = 1)
+//@Warmup(iterations = 3)
+//@Measurement(iterations = 3)
+public class JacksonCodecBenchmark {
+    private final ChannelHandlerContext ctx;
+    private final Message msg;
+    private final byte[] bytes;
+    private final JacksonCodec<Message> codec;
+
+    public JacksonCodecBenchmark() {
+        try {
+            codec = new JacksonCodec<>(Message.class);
+            ctx = mock(ChannelHandlerContext.class, Answers.RETURNS_DEEP_STUBS);
+            CompressedPublicKey sender = CompressedPublicKey.of("030944d202ce5ff0ee6df01482d224ccbec72465addc8e4578edeeaa5997f511bb");
+            CompressedPublicKey recipient = CompressedPublicKey.of("033de3da699f6f9ffbd427c56725910655ba3913be4ff55b13c628e957c860fd55");
+            byte[] payload = new byte[1024 * 1024]; // 1 MB
+            new Random().nextBytes(payload);
+            msg = new ApplicationMessage(sender, recipient, payload);
+            bytes = JACKSON_WRITER.writeValueAsBytes(msg);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Benchmark
+    public void encode() throws Exception {
+        codec.encode(ctx, msg, Unpooled.buffer());
+    }
+
+//    @Benchmark
+    public void decode() throws Exception {
+        codec.decode(ctx, Unpooled.wrappedBuffer(bytes), new ArrayList<>());
+    }
+}
