65371dfc0f874d97f58f28c6a80f7a7c2ad6b1a9
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-07-27 22:45:44 +0200

    Refactor DrasylNodeIT

--- drasyl-integration-tests/src/test/java/org/drasyl/DrasylNodeIT.java
+++ drasyl-integration-tests/src/test/java/org/drasyl/DrasylNodeIT.java
@@ -37,6 +37,7 @@ import org.drasyl.event.PeerUnreachableEvent;
 import org.drasyl.identity.CompressedPrivateKey;
 import org.drasyl.identity.CompressedPublicKey;
 import org.drasyl.identity.ProofOfWork;
+import org.drasyl.peer.connection.message.RelayableMessage;
 import org.drasyl.util.Pair;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -98,10 +99,32 @@ class DrasylNodeIT {
     }
 
     /**
-     * Two clients, one Super Peer and one Super Super Peer will be created for this test.
+     * Network Layout:
+     *
+     *        +-------+
+     *        | Super |
+     *        | Super |
+     *        | Peer  |
+     *        +---+---+
+     *            |
+     *        +---+---+
+     *        | Super |
+     *        | Peer  |
+     *        +-+--+--+
+     *          |  |
+     *     +----+  +-----+
+     *     |             |
+     * +---+----+   +----+---+
+     * |Client 1|   |Client 2|
+     * +--------+   +--------+
+     *
+     * Enabled Discovery Methods:
+     * - {@link org.drasyl.peer.connection.server.Server}
+     * - {@link org.drasyl.peer.connection.client.SuperPeerClient}
+     * - {@link org.drasyl.peer.connection.direct.DirectConnectionsManager}
      */
     @Nested
-    class SuperSuperPeerAndSuperPeerAndTwoClientWhenIntraVmDiscoveryIsDisabled {
+    class SuperSuperPeerAndSuperPeerAndTwoClientWhenOnlyNettyBasesDiscoveriesAreEnabled {
         private Pair<DrasylNode, Observable<Event>> superSuperPeer;
         private Pair<DrasylNode, Observable<Event>> superPeer;
         private Pair<DrasylNode, Observable<Event>> client1;
@@ -178,9 +201,10 @@ class DrasylNodeIT {
         }
 
         /**
-         * This test ensures that sent application messages are delivered to the recipient. All
-         * nodes send messages to every other node (including themselves). At the end, a check is
-         * made to ensure that all nodes have received all messages.
+         * This test ensures that sent application messages are delivered to the recipient (either
+         * directly or relayed via super peer or a child). All nodes send messages to every other
+         * node (including themselves). At the end, a check is made to ensure that all nodes have
+         * received all messages.
          */
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
@@ -218,6 +242,9 @@ class DrasylNodeIT {
             client2Messages.awaitCount(4);
         }
 
+        /**
+         * This test checks whether the correct {@link PeerEvent}s are emitted in the correct order.
+         */
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
         void correctPeerEventsShouldBeEmitted() {
@@ -240,6 +267,10 @@ class DrasylNodeIT {
             client2Events.awaitCount(1);
         }
 
+        /**
+         * This test checks whether the correct {@link PeerEvent}s are sent out by the other nodes
+         * when a node is shut down
+         */
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
         void shuttingDownNodeShouldCloseConnections() {
@@ -259,10 +290,26 @@ class DrasylNodeIT {
     }
 
     /**
-     * Two clients with enabled servers and one Super Peer will be created for this test.
+     * Network Layout:
+     *
+     *        +---+---+
+     *        | Super |
+     *        | Peer  |
+     *        +-+--+--+
+     *          |  |
+     *     +----+  +-----+
+     *     |             |
+     * +---+----+   +----+---+
+     * |Client 1|   |Client 2|
+     * +--------+   +--------+
+     *
+     * Enabled Discovery Methods:
+     * - {@link org.drasyl.peer.connection.server.Server}
+     * - {@link org.drasyl.peer.connection.client.SuperPeerClient}
+     * - {@link org.drasyl.peer.connection.direct.DirectConnectionsManager}
      */
     @Nested
-    class SuperPeerAndTwoClientWhenIntraVmDiscoveryIsDisabled {
+    class SuperPeerAndTwoClientWhenOnlyServerAndSuperPeerClientAndDirectConnectionsManagerAreEnabled {
         private Pair<DrasylNode, Observable<Event>> superPeer;
         private Pair<DrasylNode, Observable<Event>> client1;
         private Pair<DrasylNode, Observable<Event>> client2;
@@ -322,8 +369,11 @@ class DrasylNodeIT {
             client2.second().filter(e -> e instanceof NodeOnlineEvent || e instanceof PeerDirectEvent).test().awaitCount(2);
         }
 
+        /**
+         * This test checks that a direct connection is established when two peers communicate.
+         */
         @Test
-        void shouldEstablishDirectConnectionToOtherPeer() throws DrasylException, CryptoException {
+        void shouldEstablishDirectConnectionToOtherPeer() throws CryptoException {
             TestObserver<Event> client1RelayEvents = client1.second().filter(e -> e instanceof PeerEvent && ((PeerEvent) e).getPeer().getPublicKey().equals(CompressedPublicKey.of("025fd887836759d83b9a5e1bc565e098351fd5b86aaa184e3fb95d6598e9f9398e"))).test();
             TestObserver<Event> client2RelayEvents = client2.second().filter(e -> e instanceof PeerEvent && ((PeerEvent) e).getPeer().getPublicKey().equals(CompressedPublicKey.of("025e91733428b535e812fd94b0372c4bf2d52520b45389209acfd40310ce305ff4")) || e instanceof MessageEvent).test();
 
@@ -340,10 +390,17 @@ class DrasylNodeIT {
     }
 
     /**
-     * Four nodes with disabled sever and disabled super peer will be created for this test.
+     * Network Layout:
+     *
+     * +---+----+   +----+---+   +----+---+   +----+---+
+     * | Node 1 |   | Node 2 |   | Node 3 |   | Node 4 |
+     * +--------+   +--------+   +----+---+   +----+---+
+     *
+     * Enabled Features:
+     * - {@link org.drasyl.peer.connection.intravm.IntraVmDiscovery}
      */
     @Nested
-    class FourClientsWhenServerAndSuperPeerAreDisabled {
+    class FourNodesWithOnlyIntraVmDiscoverIsEnabled {
         private Pair<DrasylNode, Observable<Event>> node1;
         private Pair<DrasylNode, Observable<Event>> node2;
         private Pair<DrasylNode, Observable<Event>> node3;
@@ -407,13 +464,12 @@ class DrasylNodeIT {
         }
 
         /**
-         * This test ensures that sent application messages are delivered to the recipient. All
-         * nodes send messages to every other node (including themselves). At the end, a check is
-         * made to ensure that all nodes have received all messages.
+         * This test checks whether the messages sent via
+         * {@link org.drasyl.peer.connection.intravm.IntraVmDiscovery} are delivered.
          */
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
-        void applicationMessagesShouldBeDelivered() throws DrasylException {
+        void applicationMessagesShouldBeDelivered() {
             //
             // send messages
             //
@@ -447,6 +503,10 @@ class DrasylNodeIT {
             client2Messages.awaitCount(4);
         }
 
+        /**
+         * This test checks whether the {@link org.drasyl.peer.connection.intravm.IntraVmDiscovery}
+         * emits the correct {@link PeerEvent}s.
+         */
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
         void correctPeerEventsShouldBeEmitted() {
@@ -466,15 +526,18 @@ class DrasylNodeIT {
     }
 
     /**
-     * Four nodes with disabled sever, disabled super peer and disabled intra vm discovery will be
-     * created for this test.
+     * Network Layout:
+     *
+     * +---+----+
+     * | Node 1 |
+     * +--------+
+     *
+     * Enabled Discovery Methods:
+     * - {@link DrasylNode.messageSink(RelayableMessage)}
      */
     @Nested
-    class FourNodesWhenServerAndSuperPeerAndIntraVmDiscoveryAreDisabled {
+    class OneNodeWithNoDiscoveryMethodsEnabled {
         private Pair<DrasylNode, Observable<Event>> node1;
-        private Pair<DrasylNode, Observable<Event>> node2;
-        private Pair<DrasylNode, Observable<Event>> node3;
-        private Pair<DrasylNode, Observable<Event>> node4;
 
         @BeforeEach
         void setUp() throws DrasylException, CryptoException {
@@ -483,33 +546,7 @@ class DrasylNodeIT {
             //
             DrasylConfig config;
 
-            // super super peer
-            config = DrasylConfig.newBuilder()
-                    .identityProofOfWork(ProofOfWork.of(13290399))
-                    .identityPublicKey(CompressedPublicKey.of("03409386a22294ee55393eb0f83483c54f847f700df687668cc8aa3caa19a9df7a"))
-                    .identityPrivateKey(CompressedPrivateKey.of("0c2945e523e1ab27c3b38ba62f0a67a21567dcfcbad4ff3fe7f8f7b202a18c93"))
-                    .serverEnabled(false)
-                    .superPeerEnabled(false)
-                    .intraVmDiscoveryEnabled(false)
-                    .build();
-            node1 = createStartedNode(config);
-            node1.second().filter(e -> e instanceof NodeUpEvent).test().awaitCount(1);
-            colorizedPrintln("CREATED node1", COLOR_CYAN, STYLE_REVERSED);
-
-            // super peer
-            config = DrasylConfig.newBuilder()
-                    .identityProofOfWork(ProofOfWork.of(6518542))
-                    .identityPublicKey(CompressedPublicKey.of("030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22"))
-                    .identityPrivateKey(CompressedPrivateKey.of("6b4df6d8b8b509cb984508a681076efce774936c17cf450819e2262a9862f8"))
-                    .serverEnabled(false)
-                    .superPeerEnabled(false)
-                    .intraVmDiscoveryEnabled(false)
-                    .build();
-            node2 = createStartedNode(config);
-            node2.second().filter(e -> e instanceof NodeUpEvent).test().awaitCount(1);
-            colorizedPrintln("CREATED node2", COLOR_CYAN, STYLE_REVERSED);
-
-            // client1
+            // node1
             config = DrasylConfig.newBuilder()
                     .identityProofOfWork(ProofOfWork.of(12304070))
                     .identityPublicKey(CompressedPublicKey.of("025e91733428b535e812fd94b0372c4bf2d52520b45389209acfd40310ce305ff4"))
@@ -518,21 +555,9 @@ class DrasylNodeIT {
                     .superPeerEnabled(false)
                     .intraVmDiscoveryEnabled(false)
                     .build();
-            node3 = createStartedNode(config);
-            node3.second().filter(e -> e instanceof NodeUpEvent).test().awaitCount(1);
-            colorizedPrintln("CREATED node3", COLOR_CYAN, STYLE_REVERSED);
-
-            // client2
-            config = DrasylConfig.newBuilder()
-                    .identityProofOfWork(ProofOfWork.of(33957767))
-                    .identityPublicKey(CompressedPublicKey.of("025fd887836759d83b9a5e1bc565e098351fd5b86aaa184e3fb95d6598e9f9398e"))
-                    .identityPrivateKey(CompressedPrivateKey.of("0310991def7b530fced318876ac71025ebc0449a95967a0efc2e423086198f54"))
-                    .serverEnabled(false)
-                    .superPeerEnabled(false)
-                    .build();
-            node4 = createStartedNode(config);
-            node4.second().filter(e -> e instanceof NodeUpEvent).test().awaitCount(1);
-            colorizedPrintln("CREATED node4", COLOR_CYAN, STYLE_REVERSED);
+            node1 = createStartedNode(config);
+            node1.second().filter(e -> e instanceof NodeUpEvent).test().awaitCount(1);
+            colorizedPrintln("CREATED node1", COLOR_CYAN, STYLE_REVERSED);
         }
 
         /**
@@ -542,41 +567,27 @@ class DrasylNodeIT {
          */
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
-        void applicationMessagesShouldBeDelivered() throws DrasylException {
-            //
-            // send messages
-            //
+        void applicationMessagesShouldBeDelivered() {
             TestObserver<Event> superSuperPeerMessages = node1.second().filter(e -> e instanceof MessageEvent).test();
-            TestObserver<Event> superPeerMessages = node2.second().filter(e -> e instanceof MessageEvent).test();
-            TestObserver<Event> client1Messages = node3.second().filter(e -> e instanceof MessageEvent).test();
-            TestObserver<Event> client2Messages = node4.second().filter(e -> e instanceof MessageEvent).test();
-
-//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SSP: " + e));
-//        superPeer.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("SP: " + e));
-//        client1.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C1: " + e));
-//        client2.second().filter(e -> e.getCode() == MESSAGE).subscribe(e -> System.err.println("C2: " + e));
 
             node1.first().send("03409386a22294ee55393eb0f83483c54f847f700df687668cc8aa3caa19a9df7a", "Hallo Welt");
-            node2.first().send("030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22", "Hallo Welt");
-            node3.first().send("025e91733428b535e812fd94b0372c4bf2d52520b45389209acfd40310ce305ff4", "Hallo Welt");
-            node4.first().send("025fd887836759d83b9a5e1bc565e098351fd5b86aaa184e3fb95d6598e9f9398e", "Hallo Welt");
 
-            //
-            // verify
-            //
             superSuperPeerMessages.awaitCount(1);
-            superPeerMessages.awaitCount(1);
-            client1Messages.awaitCount(1);
-            client2Messages.awaitCount(1);
         }
     }
 
     /**
-     * Single non-started node.
+     * Network Layout:
+     *
+     * +---+----+
+     * | Node 1 |
+     * +--------+
+     *
+     * Non-started
      */
     @Nested
     class SingleNonStartedNode {
-        private Pair<DrasylNode, Observable<Event>> node;
+        private Pair<DrasylNode, Observable<Event>> node1;
 
         @BeforeEach
         void setUp() throws DrasylException, CryptoException {
@@ -590,20 +601,20 @@ class DrasylNodeIT {
                     .serverEnabled(false)
                     .superPeerEnabled(false)
                     .build();
-            node = createNode(config);
-            colorizedPrintln("CREATED node", COLOR_CYAN, STYLE_REVERSED);
+            node1 = createNode(config);
+            colorizedPrintln("CREATED node1", COLOR_CYAN, STYLE_REVERSED);
         }
 
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
         void sendToSelfShouldThrowException() {
-            assertThrows(ExecutionException.class, () -> node.first().send("025fd887836759d83b9a5e1bc565e098351fd5b86aaa184e3fb95d6598e9f9398e", "Hallo Welt").get());
+            assertThrows(ExecutionException.class, () -> node1.first().send("025fd887836759d83b9a5e1bc565e098351fd5b86aaa184e3fb95d6598e9f9398e", "Hallo Welt").get());
         }
 
         @Test
         @Timeout(value = TIMEOUT, unit = MILLISECONDS)
         void sendToAnOtherPeerShouldThrowException() {
-            assertThrows(ExecutionException.class, () -> node.first().send("030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22", "Hallo Welt").get());
+            assertThrows(ExecutionException.class, () -> node1.first().send("030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22", "Hallo Welt").get());
         }
     }
 }
