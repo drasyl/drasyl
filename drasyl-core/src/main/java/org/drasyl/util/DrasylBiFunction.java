d3cc289bdb0c9503db6a8772c7aec1fecd7dabae
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-09-03 21:34:33 +0200

    DrasylBiFunction added

---
+++ drasyl-core/src/main/java/org/drasyl/util/DrasylBiFunction.java
@@ -0,0 +1,20 @@
+package org.drasyl.util;
+
+/**
+ * {@link java.util.function.BiFunction} that can throw a {@link Exception}.
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <R> the type of the result of the function
+ */
+@FunctionalInterface
+public interface DrasylBiFunction<T, U, R, E extends Exception> {
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param t the first function argument
+     * @param u the second function argument
+     * @return the function result
+     */
+    R apply(T t, U u) throws E;
+}
