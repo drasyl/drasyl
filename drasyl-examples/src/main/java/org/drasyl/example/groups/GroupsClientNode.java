157cacfafb7ecbc130d7d57c514bdfea957e893a
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-10-18 21:36:50 +0200

    Added examples for Groups Plugins

---
+++ drasyl-examples/src/main/java/org/drasyl/example/groups/GroupsClientNode.java
@@ -0,0 +1,53 @@
+package org.drasyl.example.groups;
+
+import org.drasyl.DrasylConfig;
+import org.drasyl.DrasylException;
+import org.drasyl.DrasylNode;
+import org.drasyl.event.Event;
+import org.drasyl.event.NodeOnlineEvent;
+import org.drasyl.plugin.groups.client.GroupUri;
+import org.drasyl.plugin.groups.client.GroupsClientConfig;
+import org.drasyl.plugin.groups.client.GroupsClientPlugin;
+import org.drasyl.plugin.groups.client.event.GroupJoinedEvent;
+import org.drasyl.plugin.groups.manager.GroupsManagerConfig;
+import org.drasyl.plugin.groups.manager.GroupsManagerPlugin;
+import org.drasyl.plugin.groups.manager.data.Group;
+
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * This example starts a drasyl node with activated {@link GroupsClientNode}.
+ * It can be used to join groups provided by {@link GroupsManagerNode}.
+ */
+public class GroupsClientNode {
+    public static void main(final String[] args) throws DrasylException {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter URI printed by " + GroupsManagerNode.class.getSimpleName() + ": ");
+        final String input = scanner.nextLine();
+        final GroupUri group = GroupUri.of(input);
+
+        final DrasylConfig config = DrasylConfig.newBuilder()
+                .identityPath(Path.of("groups-client.identity.json"))
+                .serverBindPort(0)
+                .plugins(Set.of(new GroupsClientPlugin(
+                        GroupsClientConfig.builder()
+                                .groups(Set.of(group))
+                                .build()
+                )))
+                .build();
+
+        final DrasylNode node = new DrasylNode(config) {
+            @Override
+            public void onEvent(final Event event) {
+                System.out.println("event = " + event);
+                if (event instanceof GroupJoinedEvent) {
+                    System.out.println("Node has successfully joined group '" + ((GroupJoinedEvent) event).getGroup().getName() + "'");
+                }
+            }
+        };
+        node.start().join();
+    }
+}
---
+++ drasyl-examples/src/main/java/org/drasyl/example/groups/GroupsManagerNode.java
@@ -0,0 +1,47 @@
+package org.drasyl.example.groups;
+
+import org.drasyl.DrasylConfig;
+import org.drasyl.DrasylException;
+import org.drasyl.DrasylNode;
+import org.drasyl.event.Event;
+import org.drasyl.event.NodeOnlineEvent;
+import org.drasyl.event.NodeUpEvent;
+import org.drasyl.plugin.groups.client.GroupsClientPlugin;
+import org.drasyl.plugin.groups.manager.GroupsManagerConfig;
+import org.drasyl.plugin.groups.manager.GroupsManagerPlugin;
+import org.drasyl.plugin.groups.manager.data.Group;
+
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Set;
+
+import static java.time.Duration.ofSeconds;
+
+/**
+ * This example starts a drasyl node with activated {@link GroupsManagerPlugin}.
+ */
+public class GroupsManagerNode {
+    public static void main(final String[] args) throws DrasylException {
+        final Group group = Group.of("my-fancy-group", "s3cr3t_passw0rd!");
+        final DrasylConfig config = DrasylConfig.newBuilder()
+                .identityPath(Path.of("groups-manager.identity.json"))
+                .serverBindPort(0)
+                .plugins(Set.of(new GroupsManagerPlugin(
+                    GroupsManagerConfig.builder()
+                            .groups(Map.of(group.getName(), group))
+                            .build()
+                )))
+                .build();
+
+        final DrasylNode node = new DrasylNode(config) {
+            @Override
+            public void onEvent(final Event event) {
+                System.out.println("event = " + event);
+                if (event instanceof NodeOnlineEvent) {
+                    System.out.println("Node is online! Nodes can now join the group '" + group.getName() + "' by adding '" + group.getUri(identity().getPublicKey()).toUri() + "' to your config (drasyl.config.plugins.\"" + GroupsClientPlugin.class.getName() + "\".groups).");
+                }
+            }
+        };
+        node.start().join();
+    }
+}
