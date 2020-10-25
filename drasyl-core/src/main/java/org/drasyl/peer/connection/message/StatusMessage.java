5a18757370c244af6a5574608a9f173dab924a7a
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-10-25 22:44:51 +0100

    Removed all unused StatusMessage codes

--- drasyl-core/src/main/java/org/drasyl/peer/connection/message/StatusMessage.java
+++ drasyl-core/src/main/java/org/drasyl/peer/connection/message/StatusMessage.java
@@ -166,54 +166,17 @@ public class StatusMessage extends AbstractResponseMessage<RequestMessage> {
     }
 
     public enum Code {
-        // -------- 1xx (Informational) --------
-        STATUS_CONTINUE(100),
-        STATUS_SWITCHING_PROTOCOLS(101),
         // -------- 2xx (Successful) --------
         STATUS_OK(200),
-        STATUS_CREATED(201),
-        STATUS_ACCEPTED(202),
-        STATUS_NON_AUTHORITATIVE_INFORMATION(203),
-        STATUS_NO_CONTENT(204),
-        STATUS_RESET_CONTENT(205),
-        STATUS_PARTIAL_CONTENT(206),
-        // -------- 3xx (Redirection) --------
-        STATUS_MULTIPLE_CHOICES(300),
-        STATUS_MOVED_PERMANENTLY(301),
-        STATUS_FOUND(302),
-        STATUS_SEE_OTHER(303),
-        STATUS_NOT_MODIFIED(304),
-        STATUS_USE_PROXY(305),
-        STATUS_TEMPORARY_REDIRECT(307),
         // -------- 4xx (Client Error) --------
         STATUS_BAD_REQUEST(400),
-        STATUS_UNAUTHORIZED(401),
-        STATUS_PAYMENT_REQUIRED(402),
         STATUS_FORBIDDEN(403),
-        STATUS_NOT_FOUND(404),
-        STATUS_METHOD_NOT_ALLOWED(405),
-        STATUS_NOT_ACCEPTABLE(406),
-        STATUS_PROXY_AUTHENTICATION_REQUIRED(407),
         STATUS_REQUEST_TIMEOUT(408),
-        STATUS_CONFLICT(409),
-        STATUS_GONE(410),
-        STATUS_LENGTH_REQUIRED(411),
         STATUS_PRECONDITION_FAILED(412),
         STATUS_PAYLOAD_TOO_LARGE(413),
-        STATUS_URI_TOO_LONG(414),
-        STATUS_UNSUPPORTED_MEDIA_TYPE(415),
-        STATUS_RANGE_NOT_SATISFIABLE(416),
-        STATUS_EXPECTATION_FAILED(417),
-        STATUS_IM_A_TEAPOT(418),
-        STATUS_UPGRADE_REQUIRED(426),
         STATUS_INVALID_SIGNATURE(427),
         // -------- 5xx (Server Error) --------
-        STATUS_INTERNAL_SERVER_ERROR(500),
-        STATUS_NOT_IMPLEMENTED(501),
-        STATUS_BAD_GATEWAY(502),
-        STATUS_SERVICE_UNAVAILABLE(503),
-        STATUS_GATEWAY_TIMEOUT(504),
-        STATUS_HTTP_VERSION_NOT_SUPPORTED(505);
+        STATUS_SERVICE_UNAVAILABLE(503);
         private static final Map<Integer, Code> codes = new HashMap<>();
 
         static {
