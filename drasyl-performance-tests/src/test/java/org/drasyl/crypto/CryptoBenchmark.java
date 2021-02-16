332447b68b1e48a2ccd8e3e7b0eb01c35cb75d26
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-02-16 23:40:40 +0100

    Benchmark with different message sizes

--- drasyl-performance-tests/src/test/java/org/drasyl/crypto/CryptoBenchmark.java
+++ drasyl-performance-tests/src/test/java/org/drasyl/crypto/CryptoBenchmark.java
@@ -24,18 +24,27 @@ import org.drasyl.identity.CompressedPublicKey;
 import org.drasyl.util.RandomUtil;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+@Fork(1)
+@Warmup(iterations = 2)
+@Measurement(iterations = 2)
 @State(Scope.Benchmark)
 public class CryptoBenchmark extends AbstractBenchmark {
+    @Param({ "1", "256", "5120" })
+    private int size;
     private byte[] message;
     private PublicKey publicKey;
     private PrivateKey privateKey;
@@ -43,7 +52,7 @@ public class CryptoBenchmark extends AbstractBenchmark {
 
     @Setup
     public void setup() {
-        message = RandomUtil.randomBytes(1024);
+        message = RandomUtil.randomBytes(size);
         publicKey = CompressedPublicKey.of("030e54504c1b64d9e31d5cd095c6e470ea35858ad7ef012910a23c9d3b8bef3f22").toUncompressedKey();
         privateKey = CompressedPrivateKey.of("6b4df6d8b8b509cb984508a681076efce774936c17cf450819e2262a9862f8").toUncompressedKey();
         signature = HexUtil.fromString("304402200525a8e662d3f11fa28524de4bb83812765255db9a4d09ee5b8ede7880a54534022009416ea30daab4f8de3008c31b4ec831a0c163d08f0504b2632a2e7febdcbe06");
