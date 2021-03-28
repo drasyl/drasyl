e6179b3b6e77189a4da7d19ed1ea614717e33809
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-03-28 22:06:27 +0200

    let's try to build a native image with github workflows

--- .github/install-graal.sh
+++ .github/install-graal.sh
@@ -21,9 +21,9 @@
 # OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-GRAAL_FILENAME="graalvm-ce-java8-${GRAAL_OS}-${GRAAL_VERSION}.tar.gz"
+GRAAL_FILENAME="graalvm-ce-java${JAVA_VERSION}-${GRAAL_OS}-${GRAAL_VERSION}.tar.gz"
 
 mkdir tmp
 curl -4 -L https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAAL_VERSION}/${GRAAL_FILENAME} -o tmp/${GRAAL_FILENAME}
-tar -zxvf tmp/${GRAAL_FILENAME} -C tmp && mv tmp/graalvm-ce-java8-${GRAAL_VERSION} graalvm
+tar -zxvf tmp/${GRAAL_FILENAME} -C tmp && mv tmp/graalvm-ce-java${JAVA_VERSION}-${GRAAL_VERSION} graalvm
 rm -rf tmp/*
