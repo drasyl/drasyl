12a117414961696d9700bd42b189b2638bddb1fb
bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-05-26 12:21:30 +0000

    Preconditions

---
+++ drasyl-core/src/main/java/org/drasyl/util/Preconditions.java
@@ -0,0 +1,399 @@
+/*
+ * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.drasyl.util;
+
+/**
+ * Static convenience methods that help a method or constructor check whether it was invoked
+ * correctly (that is, whether its <i>preconditions</i> were met).
+ * <p>
+ * If the precondition is not met, the {@code Preconditions} method throws an unchecked exception of
+ * a specified type, which helps the method in which the exception was thrown communicate that its
+ * caller has made a mistake.
+ */
+public final class Preconditions {
+    public static final String MUST_BE_NON_NEGATIVE = "must be non-negative";
+    public static final String MUST_BE_POSITIVE = "must be positive";
+
+    private Preconditions() {
+        // util class
+    }
+
+    /**
+     * Checks that the specified number is non-negative. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static byte requireNonNegative(final byte obj) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(MUST_BE_NON_NEGATIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar, "bar must be non-negative");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for negativity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static byte requireNonNegative(final byte obj, final String message) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static int requireNonNegative(final int obj) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(MUST_BE_NON_NEGATIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar, "bar must be non-negative");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for negativity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static int requireNonNegative(final int obj, final String message) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static long requireNonNegative(final long obj) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(MUST_BE_NON_NEGATIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar, "bar must be non-negative");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for negativity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static long requireNonNegative(final long obj, final String message) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static short requireNonNegative(final short obj) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(MUST_BE_NON_NEGATIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is non-negative and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requireNonNegative(bar, "bar must be non-negative");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for negativity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static short requireNonNegative(final short obj, final String message) {
+        if (obj < 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static byte requirePositive(final byte obj) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(MUST_BE_POSITIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar, "bar must be positive");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for positivity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if positive
+     * @throws IllegalArgumentException if {@code obj} is not positive
+     */
+    public static byte requirePositive(final byte obj, final String message) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static int requirePositive(final int obj) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(MUST_BE_POSITIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar, "bar must be positive");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for positivity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if positive
+     * @throws IllegalArgumentException if {@code obj} is not positive
+     */
+    public static int requirePositive(final int obj, final String message) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for negativity
+     * @return {@code obj} if non-negative
+     * @throws IllegalArgumentException if {@code obj} is negative
+     */
+    public static long requirePositive(final long obj) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(MUST_BE_POSITIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar, "bar must be positive");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for positivity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if positive
+     * @throws IllegalArgumentException if {@code obj} is not positive
+     */
+    public static long requirePositive(final long obj, final String message) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive. This method is designed primarily for doing
+     * parameter validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar);
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj the number to check for positivity
+     * @return {@code obj} if positive
+     * @throws IllegalArgumentException if {@code obj} is not positive
+     */
+    public static short requirePositive(final short obj) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(MUST_BE_POSITIVE);
+        }
+
+        return obj;
+    }
+
+    /**
+     * Checks that the specified number is positive and throws a customized {@link
+     * IllegalArgumentException} if it is not. This method is designed primarily for doing parameter
+     * validation in methods and constructors, as demonstrated below:
+     * <blockquote><pre>
+     * public Foo(int bar) {
+     *     this.bar = ObjectsUtil.requirePositive(bar, "bar must be positive");
+     * }
+     * </pre></blockquote>
+     *
+     * @param obj     the number to check for positivity
+     * @param message detail message to be used in the event that a {@code IllegalArgumentException}
+     *                is thrown
+     * @return {@code obj} if positive
+     * @throws IllegalArgumentException if {@code obj} is not positive
+     */
+    public static short requirePositive(final short obj, final String message) {
+        if (obj <= 0) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return obj;
+    }
+}
---
+++ drasyl-core/src/test/java/org/drasyl/util/PreconditionsTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.drasyl.util;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.drasyl.util.Preconditions.requireNonNegative;
+import static org.drasyl.util.Preconditions.requirePositive;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class PreconditionsTest {
+    @Nested
+    class RequireNonNegative {
+        @Test
+        void shouldRejectNegativeNumbers() {
+            // byte
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative((byte) -1));
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative((byte) -1, "invalid"));
+
+            // int
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative(-1));
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative(-1, "invalid"));
+
+            // long
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative(-1L));
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative(-1L, "invalid"));
+
+            // short
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative((short) -1));
+            assertThrows(IllegalArgumentException.class, () -> requireNonNegative((short) -1, "invalid"));
+        }
+
+        @Test
+        void shouldAcceptNonNegativeNumbers() {
+            // byte
+            assertEquals((byte) 0, requireNonNegative((byte) 0));
+            assertEquals((byte) 0, requireNonNegative((byte) 0, "invalid"));
+            assertEquals((byte) 1, requireNonNegative((byte) 1));
+            assertEquals((byte) 1, requireNonNegative((byte) 1, "invalid"));
+
+            // int
+            assertEquals(0, requireNonNegative(0));
+            assertEquals(0, requireNonNegative(0, "invalid"));
+            assertEquals(1, requireNonNegative(1));
+            assertEquals(1, requireNonNegative(1, "invalid"));
+
+            // long
+            assertEquals(0L, requireNonNegative(0L));
+            assertEquals(0L, requireNonNegative(0L, "invalid"));
+            assertEquals(1L, requireNonNegative(1L));
+            assertEquals(1L, requireNonNegative(1L, "invalid"));
+
+            // short
+            assertEquals((short) 0, requireNonNegative((short) 0));
+            assertEquals((short) 0, requireNonNegative((short) 0, "invalid"));
+            assertEquals((short) 1, requireNonNegative((short) 1));
+            assertEquals((short) 1, requireNonNegative((short) 1, "invalid"));
+        }
+    }
+
+    @Nested
+    class RequirePositive {
+        @Test
+        void shouldRejectNonPositiveNumbers() {
+            // byte
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((byte) 0));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((byte) 0, "invalid"));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((byte) -1));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((byte) -1, "invalid"));
+
+            // int
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(0));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(0, "invalid"));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(-1));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(-1, "invalid"));
+
+            // long
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(0L));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(0L, "invalid"));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(-1L));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive(-1L, "invalid"));
+
+            // short
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((short) 0));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((short) 0, "invalid"));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((short) -1));
+            assertThrows(IllegalArgumentException.class, () -> requirePositive((short) -1, "invalid"));
+        }
+
+        @Test
+        void shouldAcceptPositiveNumbers() {
+            // byte
+            assertEquals(1, requirePositive((byte) 1));
+            assertEquals(1, requirePositive((byte) 1, "invalid"));
+
+            // int
+            assertEquals(1, requirePositive(1));
+            assertEquals(1, requirePositive(1, "invalid"));
+
+            // long
+            assertEquals(1L, requirePositive(1L));
+            assertEquals(1L, requirePositive(1L, "invalid"));
+
+            // short
+            assertEquals((short) 1, requirePositive((short) 1));
+            assertEquals((short) 1, requirePositive((short) 1, "invalid"));
+        }
+    }
+}
