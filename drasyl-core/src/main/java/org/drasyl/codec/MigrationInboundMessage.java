bda768642eb6d2f118a72e74bb067f4d8d1f5940
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-08-03 16:03:27 +0200

    Fix Future handling for inbound events and messages

---
+++ drasyl-core/src/main/java/org/drasyl/codec/MigrationInboundMessage.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.drasyl.codec;
+
+import org.drasyl.pipeline.Handler;
+import org.drasyl.pipeline.address.Address;
+
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A wrapper used to add {@link Handler} to a {@link io.netty.channel.Channel}.
+ */
+public class MigrationInboundMessage<T, A extends Address> {
+    private final T message;
+    private final A address;
+    private final CompletableFuture<Void> future;
+
+    public MigrationInboundMessage(final T message,
+                                   final A address,
+                                   final CompletableFuture<Void> future) {
+        this.message = message;
+        this.address = requireNonNull(address);
+        this.future = requireNonNull(future);
+    }
+
+    public T message() {
+        return message;
+    }
+
+    public A address() {
+        return address;
+    }
+
+    public CompletableFuture<Void> future() {
+        return future;
+    }
+}
---
+++ drasyl-core/src/main/java/org/drasyl/codec/MigrationOutboundMessage.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.drasyl.codec;
+
+import org.drasyl.pipeline.Handler;
+import org.drasyl.pipeline.address.Address;
+
+/**
+ * A wrapper used to add {@link Handler} to a {@link io.netty.channel.Channel}.
+ */
+public class MigrationOutboundMessage<T, A extends Address> {
+    private final T message;
+    private final A address;
+
+    public MigrationOutboundMessage(final T message,
+                                    final A address) {
+        this.message = message;
+        this.address = address;
+    }
+
+    public T message() {
+        return message;
+    }
+
+    public A address() {
+        return address;
+    }
+}
