af0dffefa50816e94cadad7752e63ab13bc7b538
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-08-16 23:28:34 +0200

    allow netty to free up read bytes

--- drasyl-core/src/main/java/org/drasyl/remote/protocol/PartialReadMessage.java
+++ drasyl-core/src/main/java/org/drasyl/remote/protocol/PartialReadMessage.java
@@ -83,68 +83,73 @@ public interface PartialReadMessage extends RemoteMessage, ReferenceCounted, Aut
     @SuppressWarnings("java:S1142")
     static PartialReadMessage of(final PublicHeader publicHeader,
                                  final ByteBuf bytes) {
-        if (publicHeader.getTotalChunks() > 0) {
-            return HeadChunkMessage.of(
-                    Nonce.of(publicHeader.getNonce()),
-                    publicHeader.getNetworkId(),
-                    IdentityPublicKey.of(publicHeader.getSender()),
-                    ProofOfWork.of(publicHeader.getProofOfWork()),
-                    IdentityPublicKey.of(publicHeader.getRecipient()),
-                    HopCount.of((byte) publicHeader.getHopCount()),
-                    UnsignedShort.of(publicHeader.getTotalChunks()),
-                    bytes
-            );
-        }
-        else if (publicHeader.getChunkNo() > 0) {
-            return BodyChunkMessage.of(
-                    Nonce.of(publicHeader.getNonce()),
-                    publicHeader.getNetworkId(),
-                    IdentityPublicKey.of(publicHeader.getSender()),
-                    ProofOfWork.of(publicHeader.getProofOfWork()),
-                    IdentityPublicKey.of(publicHeader.getRecipient()),
-                    HopCount.of((byte) publicHeader.getHopCount()),
-                    UnsignedShort.of(publicHeader.getChunkNo()),
-                    bytes
-            );
-        }
-        else if (!publicHeader.getAgreementId().isEmpty()) {
-            return ArmedMessage.of(
-                    Nonce.of(publicHeader.getNonce()),
-                    publicHeader.getNetworkId(),
-                    IdentityPublicKey.of(publicHeader.getSender()),
-                    ProofOfWork.of(publicHeader.getProofOfWork()),
-                    IdentityPublicKey.of(publicHeader.getRecipient()),
-                    HopCount.of((byte) publicHeader.getHopCount()),
-                    AgreementId.of(publicHeader.getAgreementId()),
-                    bytes
-            );
-        }
-        else {
-            final AgreementId agreementId;
-            if (publicHeader.getAgreementId().isEmpty()) {
-                agreementId = null;
+        try {
+            if (publicHeader.getTotalChunks() > 0) {
+                return HeadChunkMessage.of(
+                        Nonce.of(publicHeader.getNonce()),
+                        publicHeader.getNetworkId(),
+                        IdentityPublicKey.of(publicHeader.getSender()),
+                        ProofOfWork.of(publicHeader.getProofOfWork()),
+                        IdentityPublicKey.of(publicHeader.getRecipient()),
+                        HopCount.of((byte) publicHeader.getHopCount()),
+                        UnsignedShort.of(publicHeader.getTotalChunks()),
+                        bytes
+                );
+            }
+            else if (publicHeader.getChunkNo() > 0) {
+                return BodyChunkMessage.of(
+                        Nonce.of(publicHeader.getNonce()),
+                        publicHeader.getNetworkId(),
+                        IdentityPublicKey.of(publicHeader.getSender()),
+                        ProofOfWork.of(publicHeader.getProofOfWork()),
+                        IdentityPublicKey.of(publicHeader.getRecipient()),
+                        HopCount.of((byte) publicHeader.getHopCount()),
+                        UnsignedShort.of(publicHeader.getChunkNo()),
+                        bytes
+                );
+            }
+            else if (!publicHeader.getAgreementId().isEmpty()) {
+                return ArmedMessage.of(
+                        Nonce.of(publicHeader.getNonce()),
+                        publicHeader.getNetworkId(),
+                        IdentityPublicKey.of(publicHeader.getSender()),
+                        ProofOfWork.of(publicHeader.getProofOfWork()),
+                        IdentityPublicKey.of(publicHeader.getRecipient()),
+                        HopCount.of((byte) publicHeader.getHopCount()),
+                        AgreementId.of(publicHeader.getAgreementId()),
+                        bytes
+                );
             }
             else {
-                agreementId = AgreementId.of(publicHeader.getAgreementId());
-            }
+                final AgreementId agreementId;
+                if (publicHeader.getAgreementId().isEmpty()) {
+                    agreementId = null;
+                }
+                else {
+                    agreementId = AgreementId.of(publicHeader.getAgreementId());
+                }
 
-            final IdentityPublicKey recipient;
-            if (publicHeader.getRecipient().isEmpty()) {
-                recipient = null;
+                final IdentityPublicKey recipient;
+                if (publicHeader.getRecipient().isEmpty()) {
+                    recipient = null;
+                }
+                else {
+                    recipient = IdentityPublicKey.of(publicHeader.getRecipient());
+                }
+                return UnarmedMessage.of(
+                        Nonce.of(publicHeader.getNonce()),
+                        publicHeader.getNetworkId(),
+                        IdentityPublicKey.of(publicHeader.getSender()),
+                        ProofOfWork.of(publicHeader.getProofOfWork()),
+                        recipient,
+                        HopCount.of((byte) publicHeader.getHopCount()),
+                        agreementId,
+                        bytes
+                );
             }
-            else {
-                recipient = IdentityPublicKey.of(publicHeader.getRecipient());
-            }
-            return UnarmedMessage.of(
-                    Nonce.of(publicHeader.getNonce()),
-                    publicHeader.getNetworkId(),
-                    IdentityPublicKey.of(publicHeader.getSender()),
-                    ProofOfWork.of(publicHeader.getProofOfWork()),
-                    recipient,
-                    HopCount.of((byte) publicHeader.getHopCount()),
-                    agreementId,
-                    bytes
-            );
+        }
+        finally {
+            bytes.discardSomeReadBytes();
         }
     }
 
