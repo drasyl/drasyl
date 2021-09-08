28daee47d462c38122fb613c25833f229f7fbd5a
bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-09-08 14:47:59 +0000

    Update StopAndWaitArqHandler.java

--- drasyl-core/src/main/java/org/drasyl/channel/arq/stopandwait/StopAndWaitArqHandler.java
+++ drasyl-core/src/main/java/org/drasyl/channel/arq/stopandwait/StopAndWaitArqHandler.java
@@ -263,7 +263,7 @@ public class StopAndWaitArqHandler extends ChannelDuplexHandler {
     }
 
     private void discardPendingWrites(final Throwable cause) {
-        LOG.trace("[{}] Discard all pending writes ({}).", pendingWrites::size);
+        LOG.trace("[{}] Discard all pending writes ({}).", ctx.channel().id()::asShortText, pendingWrites::size);
         pendingWrites.removeAndFailAll(cause);
     }
 
