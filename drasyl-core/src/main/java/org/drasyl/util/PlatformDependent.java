f40782b31c5a9ef9210b39881b9ecc25d05110bc
Kevin R <dev@roebert.eu>
2021-10-18 20:48:56 +0200

    Added methods to detect platform dependent stats

--- drasyl-core/src/main/java/org/drasyl/util/PlatformDependent.java
+++ drasyl-core/src/main/java/org/drasyl/util/PlatformDependent.java
@@ -21,10 +21,16 @@
  */
 package org.drasyl.util;
 
+import com.sun.jna.Platform;
 import org.drasyl.util.logging.Logger;
 import org.drasyl.util.logging.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.net.UnknownHostException;
+import java.util.function.Function;
 
 /**
  * Utility class that can be used to detect properties specific to the current runtime environment,
@@ -33,6 +39,14 @@ import java.lang.reflect.Field;
 public final class PlatformDependent {
     private static final Logger LOG = LoggerFactory.getLogger(PlatformDependent.class);
     private static final int JAVA_VERSION = detectJavaVersion();
+    private static final String OS_NAME = detectOSName();
+    private static final String OS_VERSION = detectOSVersion();
+    private static final String OS_ARCH = detectOSArch();
+    private static final String CURRENT_USER = detectCurrentUser();
+    private static final String HOSTNAME = detectHostname();
+    private static final int AVAILABLE_PROCESSORS = detectAvailableProcessors();
+    private static final String CPU_NAME = detectCPUName();
+    private static final long TOTAL_MEMORY = detectTotalMemory();
     // See https://github.com/oracle/graal/blob/master/sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/
     // ImageInfo.java
     private static final boolean RUNNING_IN_NATIVE_IMAGE = System.getProperty("org.graalvm.nativeimage.imagecode") != null;
@@ -48,6 +62,38 @@ public final class PlatformDependent {
      */
     public static int javaVersion() {
         return JAVA_VERSION;
+    }
+
+    public static String osName() {
+        return OS_NAME;
+    }
+
+    public static String osVersion() {
+        return OS_VERSION;
+    }
+
+    public static String osArch() {
+        return OS_ARCH;
+    }
+
+    public static String currentUser() {
+        return CURRENT_USER;
+    }
+
+    public static String hostname() {
+        return HOSTNAME;
+    }
+
+    public static int availableProcessors() {
+        return AVAILABLE_PROCESSORS;
+    }
+
+    public static long totalMemory() {
+        return TOTAL_MEMORY;
+    }
+
+    public static String cpuName() {
+        return CPU_NAME;
     }
 
     /**
@@ -72,5 +118,218 @@ public final class PlatformDependent {
         LOG.debug("Java version: {}", majorVersion);
 
         return majorVersion;
+    }
+
+    private static String detectOSName() {
+        final String osName = System.getProperty("os.name", "unknown");
+
+        LOG.debug("OS name: {}", () -> osName);
+
+        return osName;
+    }
+
+    private static String detectOSVersion() {
+        final String osVersion = System.getProperty("os.version", "unknown");
+
+        LOG.debug("OS version: {}", () -> osVersion);
+
+        return osVersion;
+    }
+
+    private static String detectOSArch() {
+        final String osArch = Platform.ARCH;
+
+        LOG.debug("OS arch: {}", () -> osArch);
+
+        return osArch;
+    }
+
+    private static String detectCurrentUser() {
+        final String currentUser = System.getProperty("user.name", "unknown");
+
+        LOG.debug("Current user: {}", () -> currentUser);
+
+        return currentUser;
+    }
+
+    private static String detectHostname() {
+        String hostname;
+        try {
+            final java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
+            hostname = localMachine.getHostName();
+
+            LOG.debug("Hostname: {}", hostname);
+        }
+        catch (final UnknownHostException e) {
+            hostname = "localhost";
+        }
+
+        return hostname;
+    }
+
+    private static int detectAvailableProcessors() {
+        final int availableProcessors = Runtime.getRuntime().availableProcessors();
+
+        LOG.debug("Available processors: {}", () -> availableProcessors);
+
+        return availableProcessors;
+    }
+
+    private static long detectTotalMemory() {
+        long totalMemory = Runtime.getRuntime().maxMemory();
+
+        try {
+            if (Platform.isWindows()) {
+                final String[] commands = {
+                        "cmd",
+                        "/c",
+                        "wmic ComputerSystem get TotalPhysicalMemory | findstr [0..9]"
+                };
+                totalMemory = commandDetectionHelper(commands, Long::parseLong);
+            }
+            else if (Platform.isMac()) {
+                final String[] commands = { "system_profiler", "SPHardwareDataType" };
+
+                totalMemory = commandDetectionHelper(commands, s -> {
+                    if (s.startsWith("Memory:")) {
+                        s = s.replace("Memory", "");
+                        s = s.replace(":", "");
+                        s = s.replace(" ", "");
+                        s = s.replace("GB", "");
+
+                        return Long.parseLong(s) * 1_073_741_824;
+                    }
+
+                    return null;
+                });
+            }
+            else if (Platform.isLinux()) {
+                final String[] commands = {
+                        "/bin/bash",
+                        "-c",
+                        "cat /proc/meminfo | grep MemTotal"
+                };
+                totalMemory = commandDetectionHelper(commands, s -> {
+                    if (s.startsWith("MemTotal")) {
+                        s = s.replace("MemTotal", "");
+                        s = s.replace(":", "");
+                        s = s.replace("kB", "");
+                        s = s.replace(" ", "");
+
+                        return Long.parseLong(s) * 1_024;
+                    }
+
+                    return null;
+                });
+            }
+        }
+        catch (final Exception e) { // NOSONAR
+            // ignore
+        }
+
+        LOG.debug("Total memory: {} bytes", totalMemory);
+
+        return totalMemory;
+    }
+
+    private static String detectCPUName() {
+        String cpuName = "unknown";
+
+        try {
+            if (Platform.isWindows()) {
+                final String[] commands = {
+                        "cmd",
+                        "/c",
+                        "wmic CPU get NAME | findstr [A-Za-z]"
+                };
+                cpuName = commandDetectionHelper(commands, s -> {
+                    if (!s.startsWith("Name") && !s.isEmpty()) {
+                        return s;
+                    }
+
+                    return null;
+                });
+
+                //stdInput.readLine()
+            }
+            else if (Platform.isMac()) {
+                final String[] commands = { "system_profiler", "SPHardwareDataType" };
+
+                cpuName = commandDetectionHelper(commands, s -> {
+                    if (s.startsWith("Chip:")) {
+                        s = s.replace("Chip", "");
+                        s = s.replace(":", "");
+                        s = s.trim();
+
+                        return s;
+                    }
+
+                    return null;
+                });
+            }
+            else if (Platform.isLinux()) {
+                final String[] commands = {
+                        "/bin/bash",
+                        "-c",
+                        "cat /proc/cpuinfo | grep \"model name\""
+                };
+                cpuName = commandDetectionHelper(commands, s -> {
+                    if (s.startsWith("model name")) {
+                        s = s.replace("model name", "");
+                        s = s.replace(":", "");
+                        s = s.trim();
+
+                        return s;
+                    }
+
+                    return null;
+                });
+            }
+        }
+        catch (final Exception e) { // NOSONAR
+            // ignore
+        }
+
+        LOG.debug("CPU name: {}", cpuName);
+
+        return cpuName;
+    }
+
+    /**
+     * Executes the given {@code commands} and applies for every result line the given {@code
+     * matcher}. If the {@code matcher} returns a non-null value, it will be returned. If the {@code
+     * matcher} does not match to any line, a IOException will be thrown.
+     *
+     * @param commands the commands to execute
+     * @param matcher  the matcher to match for the wanted to be output
+     * @return the wanted output or and IOException
+     * @throws IOException if the wanted output can't be found or if an error occurred
+     */
+    private static <R> R commandDetectionHelper(final String[] commands,
+                                                final Function<String, R> matcher) throws IOException {
+        final Runtime rt = Runtime.getRuntime();
+        final Process proc = rt.exec(commands);
+
+        final BufferedReader stdError = new BufferedReader(
+                new InputStreamReader(proc.getErrorStream()));
+
+        String s;
+        if ((s = stdError.readLine()) != null) {
+            throw new IOException(s);
+        }
+
+        final BufferedReader stdInput = new BufferedReader(
+                new InputStreamReader(proc.getInputStream()));
+        while ((s = stdInput.readLine()) != null) {
+            s = s.trim();
+
+            final R result = matcher.apply(s);
+
+            if (result != null) {
+                return result;
+            }
+        }
+
+        throw new IOException();
     }
 }
