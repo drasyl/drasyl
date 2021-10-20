971fec05a87101e07c4c58319fdf54f69b657813
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-10-20 10:18:29 +0200

    fix test

--- drasyl-core/src/test/java/org/drasyl/handler/remote/LocalNetworkDiscoveryHandlerTest.java
+++ drasyl-core/src/test/java/org/drasyl/handler/remote/LocalNetworkDiscoveryHandlerTest.java
@@ -143,7 +143,7 @@ class LocalNetworkDiscoveryTest {
             verify(peer).isStale();
             verify(ctx).fireUserEventTriggered(any(RemovePathEvent.class));
             assertTrue(peers.isEmpty());
-            verify(ctx).write(argThat((ArgumentMatcher<AddressedMessage<?, ?>>) m -> m.message() instanceof DiscoveryMessage && m.address().equals(MULTICAST_ADDRESS)));
+            verify(ctx).writeAndFlush(argThat((ArgumentMatcher<AddressedMessage<?, ?>>) m -> m.message() instanceof DiscoveryMessage && m.address().equals(MULTICAST_ADDRESS)));
         }
     }
 
