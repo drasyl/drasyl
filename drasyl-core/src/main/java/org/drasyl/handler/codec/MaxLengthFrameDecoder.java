6787604958ff46fccad0ec23959eeaf39cba1588
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-11-14 17:54:47 +0100

    MaxLengthFrameDecoder added

---
+++ drasyl-core/src/main/java/org/drasyl/handler/codec/MaxLengthFrameDecoder.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.drasyl.handler.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+import static org.drasyl.util.Preconditions.requirePositive;
+
+/**
+ * A decoder that splits received {@link ByteBuf}s into frames not larger then {@link
+ * #maxFrameLength}. For example, if you received the following four fragmented packets:
+ * <pre>
+ * +---+----+-----+----+
+ * | A | BC | DEF | GH |
+ * +---+----+-----+----+
+ * </pre>
+ * A {@link MaxLengthFrameDecoder}{@code (2)} will decode them into the following three packets with
+ * the fixed length:
+ * <pre>
+ * +---+----+----+---+----+
+ * | A | BC | DE | F | GH |
+ * +---+----+----+---+----+
+ * </pre>
+ */
+public class MaxLengthFrameDecoder extends ByteToMessageDecoder {
+    private final int maxFrameLength;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param maxFrameLength the maximum length of the frame
+     */
+    public MaxLengthFrameDecoder(final int maxFrameLength) {
+        this.maxFrameLength = requirePositive(maxFrameLength);
+    }
+
+    @Override
+    protected void decode(final ChannelHandlerContext ctx,
+                          final ByteBuf in,
+                          final List<Object> out) throws Exception {
+        if (in.readableBytes() < maxFrameLength) {
+            out.add(in.readRetainedSlice(in.readableBytes()));
+        }
+        else {
+            out.add(in.readRetainedSlice(maxFrameLength));
+        }
+    }
+}
---
+++ drasyl-core/src/test/java/org/drasyl/handler/codec/MaxLengthFrameDecoderTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.drasyl.handler.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ExtendWith(MockitoExtension.class)
+class MaxLengthFrameDecoderTest {
+    @Test
+    void shouldCreateBufsWithGivenMaxFrameLength() {
+        final ChannelHandler handler = new MaxLengthFrameDecoder(2);
+        final EmbeddedChannel channel = new EmbeddedChannel(handler);
+
+        ByteBuf byteBuf = null;
+        byteBuf = Unpooled.copiedBuffer("A", UTF_8);
+        channel.writeInbound(byteBuf);
+        assertEquals(Unpooled.copiedBuffer("A", UTF_8), channel.readInbound());
+
+        byteBuf = Unpooled.copiedBuffer("BC", UTF_8);
+        channel.writeInbound(byteBuf);
+        assertEquals(Unpooled.copiedBuffer("BC", UTF_8), channel.readInbound());
+
+        byteBuf = Unpooled.copiedBuffer("DEF", UTF_8);
+        channel.writeInbound(byteBuf);
+        assertEquals(Unpooled.copiedBuffer("DE", UTF_8), channel.readInbound());
+        assertEquals(Unpooled.copiedBuffer("F", UTF_8), channel.readInbound());
+
+        byteBuf = Unpooled.copiedBuffer("GH", UTF_8);
+        channel.writeInbound(byteBuf);
+        assertEquals(Unpooled.copiedBuffer("GH", UTF_8), channel.readInbound());
+
+        assertNull(channel.readInbound());
+    }
+}
