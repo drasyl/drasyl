344e3400207427501011c2f3b16c68bf26d272d7
Kevin R <dev@roebert.eu>
2021-12-03 00:16:44 +0100

    Fix

--- drasyl-core/src/main/java/org/drasyl/handler/remote/internet/TraversingInternetDiscoveryChildrenHandler.java
+++ drasyl-core/src/main/java/org/drasyl/handler/remote/internet/TraversingInternetDiscoveryChildrenHandler.java
@@ -43,6 +43,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.function.LongSupplier;
 
 import static java.util.Objects.requireNonNull;
