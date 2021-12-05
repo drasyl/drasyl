3c1ec32932c1356c0316d290b0c5be3b9e5c8d1e
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-12-05 01:02:09 +0100

    improve paramLabel of --super-peer, rename to --super-peers

--- drasyl-cli/src/main/java/org/drasyl/cli/ChannelOptions.java
+++ drasyl-cli/src/main/java/org/drasyl/cli/ChannelOptions.java
@@ -78,9 +78,9 @@ public abstract class ChannelOptions extends GlobalOptions implements Callable<I
     )
     protected int onlineTimeoutMillis;
     @Option(
-            names = { "--super-peer" },
+            names = { "--super-peers" },
             description = "The super peers the server joins to.",
-            paramLabel = "<public-key=host:port>",
+            paramLabel = "<public-key>=<host:port>",
             defaultValue = "c0900bcfabc493d062ecd293265f571edb70b85313ba4cdda96c9f77163ba62d=sp-fra1.drasyl.org:22527,5b4578909bf0ad3565bb5faf843a9f68b325dd87451f6cb747e49d82f6ce5f4c=sp-nbg2.drasyl.org:22527",
             split = ","
     )
