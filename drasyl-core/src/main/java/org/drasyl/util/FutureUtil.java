3fccd74c5470f3bbf04d57742df1912c920880f8
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-02-16 13:58:50 +0100

    FutureUtil#synchronizeFutures stop throwing IllegalStateException if promise has already been completed

--- drasyl-core/src/main/java/org/drasyl/util/FutureUtil.java
+++ drasyl-core/src/main/java/org/drasyl/util/FutureUtil.java
@@ -93,7 +93,7 @@ public final class FutureUtil {
                 promise.setSuccess(result);
             }
             else {
-                promise.setFailure(e);
+                promise.tryFailure(e);
             }
         });
     }
