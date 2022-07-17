48a8945c920a75b4144f786052c936f4983114c9
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-07-17 15:28:08 +0200

    InconsistentSortedSet

--- drasyl-core/src/main/java/org/drasyl/util/InconsistentSortedSet.java
+++ drasyl-core/src/main/java/org/drasyl/util/InconsistentSortedSet.java
@@ -11,26 +11,26 @@ import java.util.Set;
 import java.util.SortedSet;
 
 /**
- * A {@link SortedSet} where elements are ordered using their
- * {@linkplain Comparable natural ordering}, or by a {@link Comparator} provided at set creation
- * time, depending on which constructor is used.
+ * A {@link SortedSet} where elements are ordered using their {@linkplain Comparable natural
+ * ordering}, or by a {@link Comparator} provided at set creation time, depending on which
+ * constructor is used.
  * <p>
  * Unlike {@link java.util.TreeSet}, ordering does not have to be <i>consistent with equals</i>. In
- * other words, {@link java.util.TreeSet} implies that objects are equal if
- * {@link Comparable#compareTo(Object)} returns {@code 0}. In contrast, {@link MySortedSet} uses
- * {@link Object#hashCode()} and {@link Object#equals(Object)} to determine equality.
+ * other words, {@link java.util.TreeSet} implies that objects are equal if {@link
+ * Comparable#compareTo(Object)} returns {@code 0}. In contrast, {@link InconsistentSortedSet} uses
+ * {@link Object#equals(Object)} to determine equality.
  * <p>
  * <strong>Here an example which cannot be handled by {@link java.util.TreeSet} but from
- * {@link MySortedSet}:</strong><br>Assume the DAO class {@code Person} with attributes {@code name}
- * and {@code height}. Now assume a data structure containing a set of {@code Person}s where each
- * {@code name} should be unique, and the elements should be sorted by {@code height}.
+ * {@link InconsistentSortedSet}:</strong><br>Assume the DAO class {@code Person} with attributes
+ * {@code name} and {@code height}. Now assume a data structure containing a set of {@code Person}s
+ * where each {@code name} should be unique, and the elements should be sorted by {@code height}.
  * <p>
  * Internally, this class is backed by a {@link HashSet} and {@link ArrayUtil}, so the memory
  * consumption of this class is probably equal to the sum of these two classes.
  *
  * @param <E> the type of elements maintained by this set
  */
-public class MySortedSet<E> implements SortedSet<E> {
+public class InconsistentSortedSet<E> implements SortedSet<E> {
     private final Set<E> internalSet;
     private final List<E> internalList;
     private final Comparator<? super E> comparator;
@@ -39,7 +39,7 @@ public class MySortedSet<E> implements SortedSet<E> {
      * Constructs a new, empty set; the backing {@link HashSet} and {@link ArrayList} instances have
      * the initial capacity specified by their respective implementation.
      */
-    public MySortedSet() {
+    public InconsistentSortedSet() {
         internalSet = new HashSet<>();
         internalList = new ArrayList<>();
         comparator = null;
@@ -51,7 +51,7 @@ public class MySortedSet<E> implements SortedSet<E> {
      * @param c the collection whose elements are to be placed into this set
      * @throws NullPointerException if the specified collection is {@code null}
      */
-    public MySortedSet(final Collection<? extends E> c) {
+    public InconsistentSortedSet(final Collection<? extends E> c) {
         this();
         addAll(c);
     }
@@ -63,7 +63,7 @@ public class MySortedSet<E> implements SortedSet<E> {
      * @param initialCapacity the initial capacity of the hash set and array list
      */
     @SuppressWarnings("unused")
-    public MySortedSet(final int initialCapacity) {
+    public InconsistentSortedSet(final int initialCapacity) {
         internalSet = new HashSet<>(initialCapacity);
         internalList = new ArrayList<>(initialCapacity);
         comparator = null;
@@ -71,17 +71,17 @@ public class MySortedSet<E> implements SortedSet<E> {
 
     /**
      * Constructs a new, empty set, ordered according to the given comparator.  All keys inserted
-     * into the map must be <em>mutually comparable</em> by the given comparator:
-     * {@code comparator.compare(k1, k2)} must not throw a {@code ClassCastException} for any
-     * elements {@code k1} and {@code k2} in the set. If the user attempts to put an element into
-     * the set that violates this constraint, the {@code put(Object key, Object value)} call will
-     * throw a {@code ClassCastException}.
+     * into the map must be <em>mutually comparable</em> by the given comparator: {@code
+     * comparator.compare(k1, k2)} must not throw a {@code ClassCastException} for any elements
+     * {@code k1} and {@code k2} in the set. If the user attempts to put an element into the set
+     * that violates this constraint, the {@code put(Object key, Object value)} call will throw a
+     * {@code ClassCastException}.
      *
      * @param comparator the comparator that will be used to order this set. If {@code null}, the
      *                   {@linkplain Comparable natural ordering} of the keys will be used.
      */
     @SuppressWarnings("unused")
-    public MySortedSet(final Comparator<? super E> comparator) {
+    public InconsistentSortedSet(final Comparator<? super E> comparator) {
         internalSet = new HashSet<>();
         internalList = new ArrayList<>();
         this.comparator = comparator;
@@ -199,13 +199,13 @@ public class MySortedSet<E> implements SortedSet<E> {
         else if (fromIndex > toIndex) {
             throw new IllegalArgumentException("fromElement is greater than toElement.");
         }
-        return new MySortedSet<>(internalList.subList(fromIndex, toIndex));
+        return new InconsistentSortedSet<>(internalList.subList(fromIndex, toIndex));
     }
 
     @Override
     public SortedSet<E> headSet(final E toElement) {
         final int toIndex = internalList.indexOf(toElement);
-        return new MySortedSet<>(internalList.subList(0, toIndex));
+        return new InconsistentSortedSet<>(internalList.subList(0, toIndex));
     }
 
     @Override
@@ -214,7 +214,7 @@ public class MySortedSet<E> implements SortedSet<E> {
         if (fromIndex == -1) {
             throw new IllegalArgumentException("fromElement lies outside the bounds of the range.");
         }
-        return new MySortedSet<>(internalList.subList(fromIndex, internalList.size()));
+        return new InconsistentSortedSet<>(internalList.subList(fromIndex, internalList.size()));
     }
 
     @Override
