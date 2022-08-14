652a96b5985262f97e65f19ea8713bdbb9dcc2a5
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-08-14 21:02:04 +0200

    fix javadoc

--- drasyl-core/src/main/java/org/drasyl/util/InternPool.java
+++ drasyl-core/src/main/java/org/drasyl/util/InternPool.java
@@ -23,8 +23,8 @@ package org.drasyl.util;
 
 /**
  * Represents a pool of unique objects of type {@code T}. Should only be used if objects of type
- * {@code T} are immutable and have a {@link #equals(Object)} implementation, which returns {@code
- * true} only if both objects have the same content.
+ * {@code T} are immutable and have a {@link #equals(Object)} implementation, which returns
+ * {@code true} only if both objects have the same content.
  * <p>
  * Inspired by: https://github.com/verhas/intern
  *
@@ -38,16 +38,16 @@ public class InternPool<T> {
      * <p>
      * A pool of objects of type {@code T}, initially empty, is maintained privately by this class.
      * <p>
-     * When the intern method is invoked, if the pool already contains a object equal to {@code
-     * object} as determined by the {@link #equals(Object)} method, then the object from the pool is
-     * returned. Otherwise, {@code object} is added to the pool and a reference to {@code object} is
-     * returned.
+     * When the intern method is invoked, if the pool already contains an object equal to
+     * {@code object} as determined by the {@link #equals(Object)} method, then the object from the
+     * pool is returned. Otherwise, {@code object} is added to the pool and a reference to
+     * {@code object} is returned.
      * <p>
      * It follows that for any two objects {@code a} and {@code b}, {@code intern(a) == intern(b)}
      * is {@code true} if and only if {@code a.equals(b)} is {@code true}.
      *
-     * @return a object that has the same contents as {@code object}, but is guaranteed to be from a
-     * pool of unique objects of type {@code T}.
+     * @return an object that has the same contents as {@code object}, but is guaranteed to be from
+     * a pool of unique objects of type {@code T}.
      */
     public synchronized T intern(final T object) {
         T res = pool.get(object);
