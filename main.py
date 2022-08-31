01638ae3c4fde50c8d0d87eaaa8938c6c5b19393
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-08-31 20:48:37 +0200

    libdrasyl.py

--- main.py
+++ main.py
@@ -1,105 +1,55 @@
-from ctypes import *
 import time
+import libdrasyl
 
-libdrasyl = cdll.LoadLibrary("libdrasyl.dylib")
-
-class drasyl_identity_t(Structure):
-    _fields_ = [
-        ("proof_of_work", c_int),
-        ("identity_public_key", c_char * 64),
-        ("identity_secret_key", c_char * 128)
-    ]
-
-class drasyl_node_t(Structure):
-    _fields_ = [
-        ("identity", POINTER(drasyl_identity_t))
-    ]
-
-class drasyl_peer_t(Structure):
-    _fields_ = [
-        ("address", c_char * 64)
-    ]
-
-class drasyl_event_t(Structure):
-    _fields_ = [
-        ("event_code", c_uint8),
-        ("node", POINTER(drasyl_node_t)),
-        ("peer", POINTER(drasyl_peer_t)),
-        ("message_sender", c_char * 64),
-        ("message_payload_len", c_uint16),
-        ("message_payload", c_char_p)
-    ]
-
-thread = c_void_p()
-if libdrasyl.graal_create_isolate(None, None, byref(thread)) != 0:
-    print("initialization error")
-
-def on_drasyl_event(thread, event):
+def on_event(event):
     # node events
-    if event[0].event_code == 10:
-        print("Node `%s` started." % event[0].node[0].identity[0].identity_public_key.decode('UTF-8'))
-    elif event[0].event_code == 11:
-        print("Node `%s` is shutting down." % event[0].node[0].identity[0].identity_public_key.decode('UTF-8'))
-    elif event[0].event_code == 12:
-        print("Node `%s` is now online." % event[0].node[0].identity[0].identity_public_key.decode('UTF-8'))
-    elif event[0].event_code == 13:
-        print("Node `%s` is now offline." % event[0].node[0].identity[0].identity_public_key.decode('UTF-8'))
-    elif event[0].event_code == 14:
-        print("Node `%s` failed to start." % event[0].node[0].identity[0].identity_public_key.decode('UTF-8'))
-    elif event[0].event_code == 15:
-        print("Node `%s` shut down." % event[0].node[0].identity[0].identity_public_key.decode('UTF-8'))
+    if event.event_code == libdrasyl.DRASYL_EVENT_NODE_UP:
+        print("Node `%s` started." % event.node.identity.identity_public_key)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_NODE_DOWN:
+        print("Node `%s` is shutting down." % event.node.identity.identity_public_key)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_NODE_ONLINE:
+        print("Node `%s` is now online." % event.node.identity.identity_public_key)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_NODE_OFFLINE:
+        print("Node `%s` is now offline." % event.node.identity.identity_public_key)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_NODE_UNRECOVERABLE_ERROR:
+        print("Node `%s` failed to start." % event.node.identity.identity_public_key)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_NODE_NORMAL_TERMINATION:
+        print("Node `%s` shut down." % event.node.identity.identity_public_key)
     # peer events
-    elif event[0].event_code == 20:
-        print("Direct connection to peer `%s`." % event[0].peer[0].address.decode('UTF-8'))
-    elif event[0].event_code == 21:
-        print("Relayed connection to peer `%s`." % event[0].peer[0].address.decode('UTF-8'))
-    elif event[0].event_code == 22:
-        print("Long time encryption to peer `%s`." % event[0].peer[0].address.decode('UTF-8'))
-    elif event[0].event_code == 23:
-        print("Perfect forward secrecy encryption to peer `%s`." % event[0].peer[0].address.decode('UTF-8'))
+    elif event.event_code == libdrasyl.DRASYL_EVENT_PEER_DIRECT:
+        print("Direct connection to peer `%s`." % event.peer.address)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_PEER_RELAY:
+        print("Relayed connection to peer `%s`." % event.peer.address)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_LONG_TIME_ENCRYPTION:
+        print("Long time encryption to peer `%s`." % event.peer.address)
+    elif event.event_code == libdrasyl.DRASYL_EVENT_PERFECT_FORWARD_SECRECY_ENCRYPTION:
+        print("Perfect forward secrecy encryption to peer `%s`." % event.peer.address)
     # message events
-    elif event[0].event_code == 30:
-        print("Node received from peer `%s` message `%s`" % (event[0].message_sender.decode('UTF-8'), event[0].message_payload.decode('UTF-8')))
+    elif event.event_code == libdrasyl.DRASYL_EVENT_MESSAGE:
+        print("Node received from peer `%s` message `%s`" % (event.message_sender, event.message_payload))
     # all other events
-    elif event[0].event_code == 40:
+    elif event.event_code == libdrasyl.DRASYL_EVENT_INBOUND_EXCEPTION:
         print("Node faced error while receiving message.")
     else:
-        print("Unknown event code received: %i" % event[0].event_code)
+        print("Unknown event code received: %i" % event.event_code)
 
-CMPFUNC = CFUNCTYPE(c_void_p, c_void_p, POINTER(drasyl_event_t))
-cmp_func = CMPFUNC(on_drasyl_event)
+libdrasyl.drasyl_node_init(on_event)
 
-if libdrasyl.drasyl_node_init(thread, cmp_func) != 0:
-    print("could not init node")
+identity = libdrasyl.drasyl_node_identity()
+print("My address: %s" % identity.identity_public_key)
 
-identity = POINTER(drasyl_identity_t)()
-if libdrasyl.drasyl_node_identity(thread, byref(identity)) != 0:
-    print("could not retrieve identity")
-
-print("My address: %s" % identity[0].identity_public_key.decode('UTF-8'))
-
-if libdrasyl.drasyl_node_start(thread) != 0:
-    print("could not start node")
+libdrasyl.drasyl_node_start()
 
 print("Wait for node to become online...")
-while libdrasyl.drasyl_node_is_online(thread) != 0:
-    #libdrasyl.drasyl_util_delay(thread, 50)
+while not libdrasyl.drasyl_node_is_online():
     time.sleep(0.05)
 
 recipient = "78483253e5dbbe8f401dd1bd1ef0b6f1830c46e411f611dc93a664c1e44cc054".encode("UTF-8")
 payload = "hello there".encode("UTF-8")
-if libdrasyl.drasyl_node_send(thread, recipient, payload, len(payload)) != 0:
-    print("could not send message")
+libdrasyl.drasyl_node_send(recipient, payload)
 
-#libdrasyl.drasyl_util_delay(thread, 10000)
 time.sleep(10)
 
-if libdrasyl.drasyl_node_stop(thread) != 0:
-    print("could not stop node")
-
-if libdrasyl.drasyl_shutdown_event_loop(thread) != 0:
-    print("could not shutdown event loop")
-
-libdrasyl.graal_tear_down_isolate(thread)
-
-#
+libdrasyl.drasyl_node_stop()
+libdrasyl.drasyl_shutdown_event_loop()
+libdrasyl.thread_tear_down()
