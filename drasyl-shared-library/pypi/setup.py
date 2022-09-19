bea02aa3feb536af52a7f692f5dfe0ff9620f7f9
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-09-19 23:20:42 +0200

    ?

--- drasyl-shared-library/pypi/setup.py
+++ drasyl-shared-library/pypi/setup.py
@@ -1,32 +1,21 @@
-import setuptools
-import os
-from lxml import etree as ET
+from setuptools import setup, Distribution
+from wheel.bdist_wheel import bdist_wheel
 
-try:
-    # read version from pom.xml
-    pom = ET.parse('../pom.xml', parser=ET.XMLParser(remove_comments=False))
-    version = pom.find("./{*}parent/{*}version").text
-    version = version.replace("-SNAPSHOT", ".dev0")
-except:
-    version = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+# force platform specific package wheel
+class DrasylWheel(bdist_wheel):
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
 
-# binary package
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
+class DrasylDistribution(Distribution):
+    def __init__(self, *attrs):
+        Distribution.__init__(self, *attrs)
+        self.cmdclass['bdist_wheel'] = DrasylWheel
 
-class BinaryDistribution (setuptools.Distribution):
     def has_ext_modules(self):
         return True
 
-setuptools.setup(
-    cmdclass = { 'bdist_wheel': bdist_wheel },
-    package_data = { '': ['*'] },
-    distclass = BinaryDistribution,
-    version = version
+setup(
+    # force platform specific package wheel
+    distclass = DrasylDistribution,
 )
