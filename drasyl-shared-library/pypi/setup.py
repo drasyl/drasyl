70b82f424f1eee5a68c7342e2768b3c66811d957
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-09-20 00:34:29 +0200

    package_data

--- drasyl-shared-library/pypi/setup.py
+++ drasyl-shared-library/pypi/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, Distribution
 from wheel.bdist_wheel import bdist_wheel
 
-# force platform specific package wheel
 class DrasylWheel(bdist_wheel):
     def finalize_options(self):
         bdist_wheel.finalize_options(self)
@@ -18,5 +17,11 @@ class DrasylDistribution(Distribution):
 setup(
     # force platform specific package wheel
     distclass = DrasylDistribution,
-    package_data = { '': ['*'] },
+
+    # Here we specify all the shared libs for the different platforms, but
+    # setup will probably only find the one library downloaded by the build
+    # script or placed here manually.
+    package_data = {
+        'drasyl': ['*.so*', '*.dll', '*.dylib']
+    },
 )
