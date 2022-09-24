34382a261a353fc5922f585a60ac57329366d5e2
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2022-09-24 17:10:42 +0200

    Move optional components from drasyl-core to drasyl-extras module

--- drasyl-extras/src/test/java/org/drasyl/handler/arq/stopandwait/StopAndWaitArqHandlerTest.java
+++ drasyl-extras/src/test/java/org/drasyl/handler/arq/stopandwait/StopAndWaitArqHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Heiko Bornholdt and Kevin Röbert
+ * Copyright (c) 2020-2022 Heiko Bornholdt and Kevin Röbert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
