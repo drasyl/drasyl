5e138c4e87c663d26c8364c79059650a851c4dbc
Heiko Bornholdt <heiko@bornholdt.it>
2023-03-26 00:42:55 +0100

    ???

--- drasyl-extras/src/main/java/org/drasyl/handler/connection/SegmentCodec.java
+++ drasyl-extras/src/main/java/org/drasyl/handler/connection/SegmentCodec.java
@@ -22,6 +22,7 @@
 package org.drasyl.handler.connection;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageCodec;
@@ -79,6 +80,7 @@ public class SegmentCodec extends MessageToMessageCodec<ByteBuf, Segment> {
     protected void decode(final ChannelHandlerContext ctx,
                           final ByteBuf in,
                           final List<Object> out) {
+        System.out.println(ByteBufUtil.hexDump(in));
         if (in.readableBytes() >= SEG_HDR_SIZE) {
             final int readerIndex = in.readerIndex();
 
