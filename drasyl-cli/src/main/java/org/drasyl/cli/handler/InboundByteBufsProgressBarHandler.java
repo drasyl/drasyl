9c6addd8d8316d6ef594c7a694ebaa533c7f1d80
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2023-04-06 19:59:14 -1000

    InboundByteBufsProgressBarHandler now gracefully closes progress bar when removed from pipeline

--- drasyl-cli/src/main/java/org/drasyl/cli/handler/InboundByteBufsProgressBarHandler.java
+++ drasyl-cli/src/main/java/org/drasyl/cli/handler/InboundByteBufsProgressBarHandler.java
@@ -22,8 +22,8 @@
 package org.drasyl.cli.handler;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
 import me.tongfei.progressbar.ProgressBar;
 import me.tongfei.progressbar.ProgressBarBuilder;
 import me.tongfei.progressbar.ProgressBarStyle;
@@ -36,13 +36,12 @@ import static java.util.Objects.requireNonNull;
  * Once added to the pipeline, this handler shows a progress bar keeping track of the number of
  * received readable bytes.
  */
-public class InboundByteBufsProgressBarHandler extends SimpleChannelInboundHandler<ByteBuf> {
+public class InboundByteBufsProgressBarHandler extends ChannelDuplexHandler {
     private static final DecimalFormat PROGRESS_BAR_SPEED_FORMAT = new DecimalFormat("0.00");
     private final ProgressBarBuilder progressBarBuilder;
     private ProgressBar progressBar;
 
     public InboundByteBufsProgressBarHandler(final ProgressBarBuilder progressBarBuilder) {
-        super(false);
         this.progressBarBuilder = requireNonNull(progressBarBuilder);
     }
 
@@ -58,25 +57,36 @@ public class InboundByteBufsProgressBarHandler extends SimpleChannelInboundHandl
     }
 
     @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) {
+        abort();
+    }
+
+    @Override
     public void channelInactive(final ChannelHandlerContext ctx) {
-        if (progressBar != null) {
-            progressBar.close();
-        }
+        abort();
         ctx.fireChannelInactive();
     }
 
     @Override
-    protected void channelRead0(final ChannelHandlerContext ctx,
-                                final ByteBuf msg) {
-        // create progress bar
-        if (progressBar == null) {
-            progressBar = progressBarBuilder.build();
-        }
-        progressBar.stepBy(msg.readableBytes());
-        if (progressBar.getCurrent() == progressBar.getMax()) {
-            progressBar.close();
-            ctx.pipeline().remove(this);
+    public void channelRead(final ChannelHandlerContext ctx, final Object msg) {
+        if (msg instanceof ByteBuf) {
+            // create progress bar
+            if (progressBar == null) {
+                progressBar = progressBarBuilder.build();
+            }
+            progressBar.stepBy(((ByteBuf) msg).readableBytes());
+            if (progressBar.getCurrent() == progressBar.getMax()) {
+                progressBar.close();
+                ctx.pipeline().remove(this);
+            }
         }
         ctx.fireChannelRead(msg);
+    }
+
+    private void abort() {
+        if (progressBar != null) {
+            progressBar.close();
+            progressBar = null;
+        }
     }
 }
