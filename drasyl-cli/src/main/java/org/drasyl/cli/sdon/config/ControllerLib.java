8fd763636ae97a300bdef865f06e6e92d9675819
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2024-10-25 22:06:10 +0200

    x

--- drasyl-cli/src/main/java/org/drasyl/cli/sdon/config/ControllerLib.java
+++ drasyl-cli/src/main/java/org/drasyl/cli/sdon/config/ControllerLib.java
@@ -6,19 +6,22 @@ import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.TwoArgFunction;
 
+/**
+ * Lua API provided by the controller.
+ */
 public class ControllerLib extends TwoArgFunction {
     public static NetworkTable NETWORK;
 
     @Override
     public LuaValue call(final LuaValue modname, final LuaValue env) {
         final LuaValue library = tableOf();
-        env.set("create_network", new CreateNetwork());
+        env.set("create_network", new CreateNetworkFunction());
         env.set("register_network", new RegisterNetworkFunction());
         env.set("inspect", new InspectFunction());
         return library;
     }
 
-    static class CreateNetwork extends OneArgFunction {
+    static class CreateNetworkFunction extends OneArgFunction {
         @Override
         public LuaValue call(final LuaValue paramsArg) {
             return new NetworkTable(paramsArg);
