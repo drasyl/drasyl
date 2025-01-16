f4a1fc3ddb1e9b4b1de11af4609aa370328a8ff4
Heiko <789788+HeikoBornholdt@users.noreply.github.com>
2025-01-16 19:12:34 +0100

    stop creating a new listener for each write operation (#884)

--- drasyl-performance-tests/src/main/java/org/drasyl/test/ReadThroughputBenchmarkHelper.java
+++ drasyl-performance-tests/src/main/java/org/drasyl/test/ReadThroughputBenchmarkHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024 Heiko Bornholdt and Kevin Röbert
+ * Copyright (c) 2020-2025 Heiko Bornholdt and Kevin Röbert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -98,6 +98,15 @@ public class ReadThroughputBenchmarkHelper {
 
         final InetSocketAddress remoteAddress = new InetSocketAddress(HOST, PORT);
 
+        final ChannelFutureListener listener = new ChannelFutureListener() {
+            @Override
+            public void operationComplete(final ChannelFuture future) {
+                if (!future.isSuccess() && !(future.cause() instanceof PortUnreachableException)) {
+                    future.channel().pipeline().fireExceptionCaught(future.cause());
+                }
+            }
+        };
+
         final EventLoopGroup group = new NioEventLoopGroup();
         try {
             final Bootstrap bootstrap = new Bootstrap()
@@ -116,14 +125,7 @@ public class ReadThroughputBenchmarkHelper {
                                     while (doSend && ctx.channel().isWritable()) {
                                         messagesWritten.increment();
                                         bytesWritten.add(PACKET_SIZE);
-                                        ctx.write(encoded.retain()).addListener(new ChannelFutureListener() {
-                                            @Override
-                                            public void operationComplete(final ChannelFuture future) {
-                                                if (!future.isSuccess() && !(future.cause() instanceof PortUnreachableException)) {
-                                                    future.channel().pipeline().fireExceptionCaught(future.cause());
-                                                }
-                                            }
-                                        });
+                                        ctx.write(encoded.retain()).addListener(listener);
                                     }
                                     if (!doSend) {
                                         ctx.close();
